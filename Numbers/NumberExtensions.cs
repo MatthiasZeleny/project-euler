1424f2a32ca18cbffd5acb3019c24b34a0ab47c1
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-23 17:42:10 +0200

    General: Extract even method

--- Numbers/NumberExtensions.cs
+++ Numbers/NumberExtensions.cs
@@ -3,4 +3,6 @@
 public static class NumberExtensions
 {
     public static bool IsDivisibleBy(this int number, int divisor) => number % divisor == 0;
+
+    public static bool IsEven(int number) => number.IsDivisibleBy(2);
 }
--- Problem 0002/Problem0002.cs
+++ Problem 0002/Problem0002.cs
@@ -6,10 +6,8 @@ public static class Problem0002
 {
     public static int Example()
         => GetFibonacciNumbersLessOrEqual8()
-            .Where(IsEven)
+            .Where(NumberExtensions.IsEven)
             .Sum();
-
-    private static bool IsEven(int number) => number.IsDivisibleBy(2);
 
     private static IEnumerable<int> GetFibonacciNumbersLessOrEqual8() => new List<int> { 1, 2, 3, 5, 8 };
 }
