70372ed06a505c9790f99f13e31ea469ba7c1848
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-23 18:29:35 +0200

    Problem 0002: Refactoring

--- Problem 0002.Tests/Problem0002Tests.cs
+++ Problem 0002.Tests/Problem0002Tests.cs
@@ -4,12 +4,15 @@ namespace Problem_0002.Tests;
 
 public class Problem0002Tests
 {
+    private const int ExampleResult = 10;
+    private const int SolutionResult = 0x466664;
+
     [Test]
     public void Example_ShouldReturnCorrectValue()
     {
         var example = Problem0002.Example();
 
-        example.Should().Be(10);
+        example.Should().Be(ExampleResult);
     }
 
     [Test]
@@ -17,6 +20,6 @@ public class Problem0002Tests
     {
         var solution = Problem0002.Solution();
 
-        solution.Should().Be(0x466664);
+        solution.Should().Be(SolutionResult);
     }
 }
