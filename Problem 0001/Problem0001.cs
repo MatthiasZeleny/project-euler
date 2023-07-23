27d5a56b64b90feea2be4986bff0f0543cf72234
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2023-07-23 14:59:34 +0200

    Problem 0001: Extract NumbersBelow

--- Problem 0001/Problem0001.cs
+++ Problem 0001/Problem0001.cs
@@ -9,6 +9,11 @@ public static class Problem0001
 
     private static IEnumerable<int> GetMultiplesOfThreeAndFiveBelowTen()
     {
-        return Enumerable.Range(1, 9).Where(number => number % 3 == 0 || number % 5 == 0);
+        return NumbersBelow(10).Where(number => number % 3 == 0 || number % 5 == 0);
+    }
+
+    private static IEnumerable<int> NumbersBelow(int threshold)
+    {
+        return Enumerable.Range(1, threshold - 1);
     }
 }
