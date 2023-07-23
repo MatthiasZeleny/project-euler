419d2ba40e1d80b425dc3e8dc1124565de9017da
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-23 17:57:05 +0200

    Problem 0002: Add test case and implementation

--- Numbers.Tests/FibonacciListTests.cs
+++ Numbers.Tests/FibonacciListTests.cs
@@ -5,6 +5,14 @@ namespace Numbers.Tests;
 public class FibonacciListTests
 {
     [Test]
+    public void GetAllLessOrEqual_One_ShouldReturnCorrectList()
+    {
+        var list = Fibonacci.GetAllLessOrEqual(1);
+
+        list.Should().BeEquivalentTo(new List<int> { 1 });
+    }
+
+    [Test]
     public void GetAllLessOrEqual_Eight_ShouldReturnCorrectList()
     {
         var list = Fibonacci.GetAllLessOrEqual(8);
--- Numbers/Fibonacci.cs
+++ Numbers/Fibonacci.cs
@@ -4,6 +4,11 @@ public static class Fibonacci
 {
     public static IEnumerable<int> GetAllLessOrEqual(int threshold)
     {
+        if (threshold == 1)
+        {
+            return new List<int> { 1 };
+        }
+
         return new List<int> { 1, 2, 3, 5, threshold };
     }
 }
