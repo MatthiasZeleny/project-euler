21cebb87e00b84793df10a68b43a7e5f0b07fcb2
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-23 18:17:49 +0200

    Problem 0002: Solve problem

--- Problem 0002.Tests/Problem0002Tests.cs
+++ Problem 0002.Tests/Problem0002Tests.cs
@@ -5,10 +5,18 @@ namespace Problem_0002.Tests;
 public class Problem0002Tests
 {
     [Test]
-    public void Test1()
+    public void Example_ShouldReturnCorrectValue()
     {
         var example = Problem0002.Example();
 
         example.Should().Be(10);
+    }
+
+    [Test]
+    public void Solution_ShouldReturnCorrectValue()
+    {
+        var solution = Problem0002.Solution();
+
+        solution.Should().Be(0x466664);
     }
 }
--- Problem 0002/Problem0002.cs
+++ Problem 0002/Problem0002.cs
@@ -6,6 +6,8 @@ public static class Problem0002
 {
     public static int Example() => GetSumOfEvenFibonacciNumbersUpTo(8);
 
+    public static int Solution() => GetSumOfEvenFibonacciNumbersUpTo(4_000_000);
+
     private static int GetSumOfEvenFibonacciNumbersUpTo(int threshold)
     {
         return Fibonacci.GetAllLessOrEqual(threshold)
