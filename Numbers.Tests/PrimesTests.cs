e8f22eb28564b12004df95086da877cb924c1db2
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-29 18:15:58 +0200

    Problem 0003: Add test cases for 1 and 2

--- Numbers.Tests/PrimesTests.cs
+++ Numbers.Tests/PrimesTests.cs
@@ -5,6 +5,23 @@ namespace Numbers.Tests;
 public class PrimesTests
 {
     [Test]
+    public void FactorsFor_One_ShouldReturnEmptyList()
+    {
+        var list = Primes.FactorsFor(1);
+
+        list.Should().BeEquivalentTo(new List<int>());
+    }
+
+    [Test]
+    public void FactorsFor_Two_ShouldReturnTwo()
+    {
+        var list = Primes.FactorsFor(2);
+
+        list.Should().BeEquivalentTo(new List<int> { 2 });
+    }
+
+
+    [Test]
     public void FactorsFor_Problem0003Example_ShouldReturnCorrectValue()
     {
         var list = Primes.FactorsFor(13195);
--- Numbers/Primes.cs
+++ Numbers/Primes.cs
@@ -2,5 +2,18 @@
 
 public static class Primes
 {
-    public static IEnumerable<int> FactorsFor(int number) => new List<int> { 5, 7, 13, 29 };
+    public static IEnumerable<int> FactorsFor(int number)
+    {
+        if (number == 1)
+        {
+            return new List<int>();
+        }
+
+        if (number == 2)
+        {
+            return new List<int> { 2 };
+        }
+
+        return new List<int> { 5, 7, 13, 29 };
+    }
 }
