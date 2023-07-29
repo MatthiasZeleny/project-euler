86db8553cc919378be96ac1c22d30b2f71b1b7a4
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-29 16:45:14 +0200

    Problem 0003: Extract prime factor class

---
+++ Numbers/Primes.cs
@@ -0,0 +1,6 @@
+﻿namespace Numbers;
+
+public static class Primes
+{
+    public static IEnumerable<int> For(int number) => new List<int> { 5, 7, 13, 29 };
+}
--- Problems/Problem0003.cs
+++ Problems/Problem0003.cs
@@ -1,8 +1,8 @@
-﻿namespace Problems;
+﻿using Numbers;
+
+namespace Problems;
 
 public static class Problem0003
 {
-    public static int Example() => GetPrimeFactorsFor13195().Last();
-
-    private static IEnumerable<int> GetPrimeFactorsFor13195() => new List<int> { 5, 7, 13, 29 };
+    public static int Example() => Primes.For(13195).Last();
 }
