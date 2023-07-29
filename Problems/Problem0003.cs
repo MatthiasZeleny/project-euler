26729aca077d3ace2105b05a4c5d9677bcaa710f
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-29 18:48:00 +0200

    Problem 0003: Refactoring

--- Problems/Problem0003.cs
+++ Problems/Problem0003.cs
@@ -4,5 +4,7 @@ namespace Problems;
 
 public static class Problem0003
 {
-    public static int Example() => Primes.FactorsFor(13195).Last();
+    public static int Example() => GetBiggestPrimeFactorFor(13195);
+
+    private static int GetBiggestPrimeFactorFor(int number) => Primes.FactorsFor(number).Last();
 }
