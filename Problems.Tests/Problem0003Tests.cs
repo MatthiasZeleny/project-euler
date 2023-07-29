7133fb1f6cb0687b67a8ba1128157dbf5c938f5e
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-29 19:04:37 +0200

    Problem 0003: Solve problem 0003

--- Problems.Tests/Problem0003Tests.cs
+++ Problems.Tests/Problem0003Tests.cs
@@ -4,7 +4,8 @@ namespace Problems.Tests;
 
 public class Problem0003Tests
 {
-    private const int ExampleResult = 29;
+    private const long ExampleResult = 29;
+    private const long ProblemResult = 0x1AC9;
 
     [Test]
     public void Example_ShouldReturnCorrectValue()
@@ -12,5 +13,13 @@ public class Problem0003Tests
         var example = Problem0003.Example();
 
         example.Should().Be(ExampleResult);
+    }
+    
+    [Test]
+    public void Solution_ShouldReturnCorrectValue()
+    {
+        var example = Problem0003.Solution();
+
+        example.Should().Be(ProblemResult);
     }
 }
--- Problems/Problem0003.cs
+++ Problems/Problem0003.cs
@@ -6,5 +6,7 @@ public static class Problem0003
 {
     public static long Example() => GetBiggestPrimeFactorFor(13195);
 
+    public static long Solution() => GetBiggestPrimeFactorFor(600851475143);
+
     private static long GetBiggestPrimeFactorFor(long number) => Primes.FactorsFor(number).Last();
 }
