faa12a9b5287cc18df80d47e45c33164944f0cd5
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-29 16:40:09 +0200

    Problem 0003: Add example prime factor list

--- Problems/Problem0003.cs
+++ Problems/Problem0003.cs
@@ -2,5 +2,7 @@
 
 public static class Problem0003
 {
-    public static int Example() => 29;
+    public static int Example() => GetPrimeFactorsFor13195().Last();
+
+    private static IEnumerable<int> GetPrimeFactorsFor13195() => new List<int> { 5, 7, 13, 29 };
 }
