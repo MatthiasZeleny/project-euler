405874ee71efb638986515e58739769eee7d8722
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 16:47:37 +0200

    Problem 0005: Refactoring

--- Problems/Problem0005.cs
+++ Problems/Problem0005.cs
@@ -5,10 +5,15 @@ namespace Problems;
 
 public class Problem0005 : IEulerProblem
 {
-    public long Example() => NumberList.NumbersUpTo(10).Then(GetLeastCommonMultiple);
+    public long Example() => GetLeastCommonMultipleOfNumbersUpTo(10);
+
+    private static long GetLeastCommonMultipleOfNumbersUpTo(int maxValue) =>
+        NumberList.NumbersUpTo(maxValue)
+            .Then(GetLeastCommonMultiple);
 
     public long Solution() => 0;
 
-    private static long GetLeastCommonMultiple(IEnumerable<long> numbers) => PrimeFactorRepresentation.GetLeastCommon(numbers)
+    private static long GetLeastCommonMultiple(IEnumerable<long> numbers) => PrimeFactorRepresentation
+        .GetLeastCommon(numbers)
         .AsNumber();
 }
