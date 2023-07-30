3ceb50a06aa7d28a91a7744cb485c592e6eca715
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 16:11:49 +0200

    Problem 0005: Move PrimeFactorRepresentationTests into own file

---
+++ Numbers.Tests/PrimeFactorRepresentationTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace Numbers.Tests;
+
+public class PrimeFactorRepresentationTests
+{
+    [Test]
+    public void AsList_One_ShouldReturnEmptyList()
+    {
+        var list = PrimeFactorRepresentation.For(1).AsList();
+
+        list.Should().BeEquivalentTo(new List<int>());
+    }
+
+    [Test]
+    public void AsList_Two_ShouldReturnTwo()
+    {
+        var list = PrimeFactorRepresentation.For(2).AsList();
+
+        list.Should().BeEquivalentTo(new List<int> { 2 });
+    }
+
+    [Test]
+    public void AsList_Four_ShouldReturnTwoTwos()
+    {
+        var list = PrimeFactorRepresentation.For(4).AsList();
+
+        list.Should().BeEquivalentTo(new List<int> { 2, 2 });
+    }
+
+
+    [Test]
+    public void AsList_Problem0003Example_ShouldReturnCorrectValue()
+    {
+        var list = PrimeFactorRepresentation.For(13195).AsList();
+
+        list.Should().BeEquivalentTo(new List<int> { 5, 7, 13, 29 });
+    }
+}
--- Numbers.Tests/PrimesTests.cs
+++ Numbers.Tests/PrimesTests.cs
@@ -20,39 +20,3 @@ public class PrimesTests
         primes.Take(10).Should().BeEquivalentTo(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 });
     }
 }
-
-public class PrimeFactorRepresentationTests
-{
-    [Test]
-    public void AsList_One_ShouldReturnEmptyList()
-    {
-        var list = PrimeFactorRepresentation.For(1).AsList();
-
-        list.Should().BeEquivalentTo(new List<int>());
-    }
-
-    [Test]
-    public void AsList_Two_ShouldReturnTwo()
-    {
-        var list = PrimeFactorRepresentation.For(2).AsList();
-
-        list.Should().BeEquivalentTo(new List<int> { 2 });
-    }
-
-    [Test]
-    public void AsList_Four_ShouldReturnTwoTwos()
-    {
-        var list = PrimeFactorRepresentation.For(4).AsList();
-
-        list.Should().BeEquivalentTo(new List<int> { 2, 2 });
-    }
-
-
-    [Test]
-    public void AsList_Problem0003Example_ShouldReturnCorrectValue()
-    {
-        var list = PrimeFactorRepresentation.For(13195).AsList();
-
-        list.Should().BeEquivalentTo(new List<int> { 5, 7, 13, 29 });
-    }
-}
