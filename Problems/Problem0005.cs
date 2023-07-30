407102790dca2a2d376c2d418b910bdf7957de8e
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 16:47:49 +0200

    Problem 0005: Order methods

--- Problems/Problem0005.cs
+++ Problems/Problem0005.cs
@@ -7,11 +7,11 @@ public class Problem0005 : IEulerProblem
 {
     public long Example() => GetLeastCommonMultipleOfNumbersUpTo(10);
 
+    public long Solution() => 0;
+
     private static long GetLeastCommonMultipleOfNumbersUpTo(int maxValue) =>
         NumberList.NumbersUpTo(maxValue)
             .Then(GetLeastCommonMultiple);
-
-    public long Solution() => 0;
 
     private static long GetLeastCommonMultiple(IEnumerable<long> numbers) => PrimeFactorRepresentation
         .GetLeastCommon(numbers)
