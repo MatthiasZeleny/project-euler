b8b8ad3bcb7d9b976f09623b88add48b960e0ba4
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 15:08:27 +0200

    Problem 0004: Add dummy code for example

---
+++ Problems.Tests/Problem0005Tests.cs
@@ -0,0 +1,7 @@
+﻿namespace Problems.Tests;
+
+public class Problem0005Tests : EulerProblemTestBase<Problem0005>
+{
+    protected override long ExampleResult => 2520;
+    protected override long ProblemResult => 0xD000;
+}
---
+++ Problems/Problem0005.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems;
+
+public class Problem0005 : IEulerProblem
+{
+    public long Example() => 2520;
+
+    public long Solution() => 0;
+}
