4c7830561103e27d4141e873d4e6ff6bde7c6ea4
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 17:37:47 +0200

    Problem 0006: Use squared for example

--- Problems/Problem0006.cs
+++ Problems/Problem0006.cs
@@ -1,8 +1,18 @@
-﻿namespace Problems;
+﻿using Numbers;
+
+namespace Problems;
 
 public class Problem0006 : IEulerProblem
 {
-    public long Example() => 3025 - 385;
+    public long Example()
+    {
+        var numbers = NumberList.NumbersUpTo(10).ToList();
+
+        var sumOfSquared = numbers.Select(number => number.Squared()).Sum();
+        var squaredOfSum = numbers.Sum().Squared();
+
+        return squaredOfSum - sumOfSquared;
+    }
 
     public long Solution() => 0;
 }
