0d4f43bae5835f14cddb9ce43388b80045b11e2a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-07-30 17:02:06 +0200

    Problem 0006: Start problem with example dummy

---
+++ Problems.Tests/Problem0006Tests.cs
@@ -0,0 +1,7 @@
+﻿namespace Problems.Tests;
+
+public class Problem0006Tests : EulerProblemTestBase<Problem0006>
+{
+    protected override long ExampleResult => 2640;
+    protected override long ProblemResult => 0x0;
+}
---
+++ Problems/Problem0006.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems;
+
+public class Problem0006 : IEulerProblem
+{
+    public long Example() => 2640;
+
+    public long Solution() => 0;
+}
