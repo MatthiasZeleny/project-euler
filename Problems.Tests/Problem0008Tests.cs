80de4ebb1ab55b6cd6e02340cc25eeb6306d5b8a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-06 18:06:21 +0200

    Problem 0008: Use method name explaining problem

--- Problems.Tests/Problem0008Tests.cs
+++ Problems.Tests/Problem0008Tests.cs
@@ -2,9 +2,11 @@
 
 public class Problem0008Tests : EulerProblemTestBase<Problem0008>
 {
-    protected override long ExampleResult =>
-        new List<long> { 9, 9, 8, 9 }
-            .Aggregate(1l, (product, factor) => product * factor);
+    protected override long ExampleResult => GetLargestProductInMatrixUsingNDigits(4);
 
     protected override long ProblemResult => 0x0;
+
+    private static long GetLargestProductInMatrixUsingNDigits(int numberOfDigits) =>
+        new List<long> { 9, 9, 8, 9 }
+            .Aggregate(1L, (product, factor) => product * factor);
 }
