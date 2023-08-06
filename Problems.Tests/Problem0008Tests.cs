0e5d2d3b6886fdad9a26155f22ca44e34e90527f
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-06 17:58:35 +0200

    Problem 0008: Use product

--- Problems.Tests/Problem0008Tests.cs
+++ Problems.Tests/Problem0008Tests.cs
@@ -2,6 +2,6 @@
 
 public class Problem0008Tests : EulerProblemTestBase<Problem0008>
 {
-    protected override long ExampleResult => 5832;
+    protected override long ExampleResult => 9 * 9 * 8 * 9;
     protected override long ProblemResult => 0x0;
 }
