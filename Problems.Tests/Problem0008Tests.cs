b2aee7f88d90f8d7c472596eebbfa75181bba0c9
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-06 18:01:19 +0200

    Problem 0008: Create List for product

--- Problems.Tests/Problem0008Tests.cs
+++ Problems.Tests/Problem0008Tests.cs
@@ -2,6 +2,9 @@
 
 public class Problem0008Tests : EulerProblemTestBase<Problem0008>
 {
-    protected override long ExampleResult => 9 * 9 * 8 * 9;
+    protected override long ExampleResult =>
+        new List<long> { 9, 9, 8, 9 }
+            .Aggregate(1l, (product, factor) => product * factor);
+
     protected override long ProblemResult => 0x0;
 }
