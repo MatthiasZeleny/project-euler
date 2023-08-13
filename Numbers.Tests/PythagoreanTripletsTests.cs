989fe161d03bff45ca4b6175c58179676d60b813
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:57:49 +0200

    Problem 0009: Add test for dummy implementation

--- Numbers.Tests/PythagoreanTripletsTests.cs
+++ Numbers.Tests/PythagoreanTripletsTests.cs
@@ -11,4 +11,13 @@ public class PythagoreanTripletsTests
 
         triplet.Should().Be((3, 4, 5));
     }
+
+
+    [Test]
+    public void GetTripletsUpTill_Five_ReturnsSingleTriplet()
+    {
+        var triplet = PythagoreanTriplets.GetTripletsUpTill(5);
+
+        triplet.Should().BeEquivalentToWithStrictOrdering(new List<(long a, long b, long c)> { (3, 4, 5) });
+    }
 }
--- Numbers/PythagoreanTriplets.cs
+++ Numbers/PythagoreanTriplets.cs
@@ -4,5 +4,5 @@ public static class PythagoreanTriplets
 {
     public static (long a, long b, long c) CreateTripledWithSum(long sum) => GetTripletsUpTill(sum).First();
 
-    private static List<(long a, long b, long c)> GetTripletsUpTill(long sum) => new() { (3, 4, 5) };
+    public static List<(long a, long b, long c)> GetTripletsUpTill(long sum) => new() { (3, 4, 5) };
 }
