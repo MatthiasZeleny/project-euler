710e1ea8e5b2a7b28a1671d84b5fe98eecf36157
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:53:56 +0200

    Problem 0009: Refactoring

--- Numbers/PythagoreanTriplets.cs
+++ Numbers/PythagoreanTriplets.cs
@@ -2,5 +2,7 @@
 
 public static class PythagoreanTriplets
 {
-    public static (long a, long b, long c) CreateTripledWithSum(long sum) => (3, 4, 5);
+    public static (long a, long b, long c) CreateTripledWithSum(long sum) => GetTripletsUpTill(sum).First();
+
+    private static List<(long a, long b, long c)> GetTripletsUpTill(long sum) => new() { (3, 4, 5) };
 }
