79e71452ea4441e8e5a9ef4dfd57e27633c0b595
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:52:52 +0200

    Problem 0009: add test for dummy implementation

---
+++ Numbers.Tests/PythagoreanTripletsTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+
+namespace Numbers.Tests;
+
+public class PythagoreanTripletsTests
+{
+    [TestCase(3 + 4 + 5, 3, 4, 5)]
+    public void CreateTripledWithSum(long sum, long a, long b, long c)
+    {
+        var triplet = PythagoreanTriplets.CreateTripledWithSum(sum);
+
+        triplet.Should().Be((3, 4, 5));
+    }
+}
--- Numbers/PythagoreanTriplets.cs
+++ Numbers/PythagoreanTriplets.cs
@@ -2,5 +2,5 @@
 
 public static class PythagoreanTriplets
 {
-    public static (long a, long b, long c) CreateTripledWithSum(int sum) => (3, 4, 5);
+    public static (long a, long b, long c) CreateTripledWithSum(long sum) => (3, 4, 5);
 }
