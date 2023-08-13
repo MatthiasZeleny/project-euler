22aae9ac91c616059e832029ca76e076babd43ec
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:54:54 +0200

    Problem 0009: Fix test name

--- Numbers.Tests/PythagoreanTripletsTests.cs
+++ Numbers.Tests/PythagoreanTripletsTests.cs
@@ -5,7 +5,7 @@ namespace Numbers.Tests;
 public class PythagoreanTripletsTests
 {
     [TestCase(3 + 4 + 5, 3, 4, 5)]
-    public void CreateTripledWithSum(long sum, long a, long b, long c)
+    public void CreateTripledWithSum_ShouldReturnFittingTriplet(long sum, long a, long b, long c)
     {
         var triplet = PythagoreanTriplets.CreateTripledWithSum(sum);
 
