0ab17946e76eded8239755f0620e63da4080d642
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:47:38 +0200

    Problem 0009: Use sum of triplet

--- Problems/Problem0009.cs
+++ Problems/Problem0009.cs
@@ -2,7 +2,16 @@
 
 public class Problem0009 : IEulerProblem
 {
-    public long Example() => 3 * 4 * 5;
+    public long Example() => GetProductOfPythagoreanTripletWithSum(3 + 4 + 5);
+
+    private long GetProductOfPythagoreanTripletWithSum(int sum)
+    {
+        var (a, b, c) = CreateTripledWithSum(sum);
+
+        return a * b * c;
+    }
+
+    private (long a, long b, long c) CreateTripledWithSum(int sum) => (3, 4, 5);
 
     public long Solution() => 0;
 }
