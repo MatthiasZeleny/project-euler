2a18f613b53a8c99e82591d274c19a5938d68104
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:49:06 +0200

    Problem 0009: Make methods static

--- Problems/Problem0009.cs
+++ Problems/Problem0009.cs
@@ -4,14 +4,14 @@ public class Problem0009 : IEulerProblem
 {
     public long Example() => GetProductOfPythagoreanTripletWithSum(3 + 4 + 5);
 
-    private long GetProductOfPythagoreanTripletWithSum(int sum)
+    private static long GetProductOfPythagoreanTripletWithSum(int sum)
     {
         var (a, b, c) = CreateTripledWithSum(sum);
 
         return a * b * c;
     }
 
-    private (long a, long b, long c) CreateTripledWithSum(int sum) => (3, 4, 5);
+    private static (long a, long b, long c) CreateTripledWithSum(int sum) => (3, 4, 5);
 
     public long Solution() => 0;
 }
