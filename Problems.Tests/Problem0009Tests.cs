e741388dde1072756bd61a105fa0ab6205b0d095
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 19:17:15 +0200

    Problem 0009: Solve problem

--- Problems.Tests/Problem0009Tests.cs
+++ Problems.Tests/Problem0009Tests.cs
@@ -4,5 +4,5 @@ public class Problem0009Tests : EulerProblemTestBase<Problem0009>
 {
     protected override long ExampleResult => 3 * 4 * 5;
 
-    protected override long ProblemResult => 0x0;
+    protected override long ProblemResult => 0x1E65FB8;
 }
--- Problems/Problem0009.cs
+++ Problems/Problem0009.cs
@@ -6,12 +6,12 @@ public class Problem0009 : IEulerProblem
 {
     public long Example() => GetProductOfPythagoreanTripletWithSum(3 + 4 + 5);
 
+    public long Solution() => GetProductOfPythagoreanTripletWithSum(1000);
+
     private static long GetProductOfPythagoreanTripletWithSum(int sum)
     {
         var (a, b, c) = PythagoreanTriplets.CreateTripledWithSum(sum);
 
         return a * b * c;
     }
-
-    public long Solution() => 0;
 }
