da96dffa73215083bdc7be9b94e6b88b8df680bc
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 19:16:03 +0200

    Problem 0009: Add test case

--- Numbers.Tests/PythagoreanTripletsTests.cs
+++ Numbers.Tests/PythagoreanTripletsTests.cs
@@ -5,11 +5,12 @@ namespace Numbers.Tests;
 public class PythagoreanTripletsTests
 {
     [TestCase(3 + 4 + 5, 3, 4, 5)]
+    [TestCase(9 + 12 + 15, 9, 12, 15)]
     public void CreateTripledWithSum_ShouldReturnFittingTriplet(long sum, long a, long b, long c)
     {
         var triplet = PythagoreanTriplets.CreateTripledWithSum(sum);
 
-        triplet.Should().Be((3, 4, 5));
+        triplet.Should().Be((a, b, c));
     }
 
 
--- Numbers/PythagoreanTriplets.cs
+++ Numbers/PythagoreanTriplets.cs
@@ -2,7 +2,8 @@
 
 public static class PythagoreanTriplets
 {
-    public static (long a, long b, long c) CreateTripledWithSum(long sum) => GetTripletsUpTill(sum).First();
+    public static (long a, long b, long c) CreateTripledWithSum(long sum) =>
+        GetTripletsUpTill(sum).First(tuple => tuple.a + tuple.b + tuple.c == sum);
 
     public static IEnumerable<(long a, long b, long c)> GetTripletsUpTill(long highestPossibleNumber)
     {
