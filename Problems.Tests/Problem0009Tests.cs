6be848aadb90da953750bd8bac17e2fa6757c298
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-08-13 18:42:32 +0200

    Problem 0009: Create problem with example

---
+++ Problems.Tests/Problem0009Tests.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems.Tests;
+
+public class Problem0009Tests : EulerProblemTestBase<Problem0009>
+{
+    protected override long ExampleResult => 3 * 4 * 5;
+
+    protected override long ProblemResult => 0x0;
+}
---
+++ Problems/Problem0009.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems;
+
+public class Problem0009 : IEulerProblem
+{
+    public long Example() => 3 * 4 * 5;
+
+    public long Solution() => 0;
+}
