b9199ed2f19e0c9c75ee4c935f5a17c633587f30
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-10 09:28:13 +0200

    Problem 0011: Solve dummy problem

---
+++ Problems.Tests/Problem0011Tests.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems.Tests;
+
+public class Problem0011Tests : EulerProblemTestBase<Problem0011>
+{
+    protected override long ExampleResult => 99 * 97;
+
+    protected override long ProblemResult => 0;
+}
---
+++ Problems/Problem0011.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems;
+
+public class Problem0011 : IEulerProblem
+{
+    public long Example() => 99 * 97;
+
+    public long Solution() => 0;
+}
