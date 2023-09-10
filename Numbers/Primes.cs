c3cc50f62d5a6f67e169d32939f6a629c8336b61
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-10 10:53:36 +0200

    General: Try improve performance for large primes, again
    This time, the idea is using prime wheel factorization.

--- Numbers/Primes.cs
+++ Numbers/Primes.cs
@@ -2,22 +2,40 @@
 
 public static class Primes
 {
+    private static readonly IReadOnlyCollection<long> PrimeFactorsStartForThirty = new List<long>
+        { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+    private static readonly IReadOnlyCollection<long> WheelFactorizationCandidatesForBaseThirty = new List<long>
+        { 1, 7, 11, 13, 17, 19, 23, 29 };
+
+    private static readonly long Thirty = 2 * 3 * 5;
+
     public static IEnumerable<long> Create()
     {
-        var naturals = NumberList.NaturalNumbers()
-            .Skip(1);
+        var naturals = NumberList.NaturalNumbers();
         var primes = new List<long>();
+
+        foreach (var prime in PrimeFactorsStartForThirty)
+        {
+            primes.Add(prime);
+            yield return prime;
+        }
 
         foreach (var natural in naturals)
         {
-            if (primes.Any(prime => natural.IsDivisibleBy(prime)))
+            foreach (var wheelFactorizationCandidate in WheelFactorizationCandidatesForBaseThirty)
             {
-                continue;
+                var candidate = Thirty * natural + wheelFactorizationCandidate;
+
+                if (primes.Any(prime => candidate.IsDivisibleBy(prime)))
+                {
+                    continue;
+                }
+
+                primes.Add(candidate);
+
+                yield return candidate;
             }
-
-            primes.Add(natural);
-
-            yield return natural;
         }
     }
 }
