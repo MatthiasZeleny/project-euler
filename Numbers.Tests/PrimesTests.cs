ceb277cc782964671e1756f7ec8b5003a2e26dd4
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-10 10:53:19 +0200

    General: Add test

--- Numbers.Tests/PrimesTests.cs
+++ Numbers.Tests/PrimesTests.cs
@@ -19,4 +19,13 @@ public class PrimesTests
 
         primes.Take(10).Should().BeEquivalentTo(new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 });
     }
+
+    [Test]
+    public void Create_TakeBelowHundred_ShouldReturnCorrectValues()
+    {
+        var primes = Primes.Create();
+
+        primes.TakeWhile(prime => prime < 100).Should().BeEquivalentTo(new List<int>
+            { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 });
+    }
 }
