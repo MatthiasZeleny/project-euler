c913b71ef286ee051889a8b236dce4847503046e
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-10 10:54:29 +0200

    Revert "General: Try improve performance for large primes, again"
    This reverts commit c3cc50f62d5a6f67e169d32939f6a629c8336b61.
    
    The factorization is based on removing certain candidates earlier based on being divisible some low primes.
    This does not seem to change that much performance, since the prime check is fast for those anyway.
    The check uses the found primes in ascending order.

--- Numbers/Primes.cs
+++ Numbers/Primes.cs
@@ -2,40 +2,22 @@
 
 public static class Primes
 {
-    private static readonly IReadOnlyCollection<long> PrimeFactorsStartForThirty = new List<long>
-        { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-
-    private static readonly IReadOnlyCollection<long> WheelFactorizationCandidatesForBaseThirty = new List<long>
-        { 1, 7, 11, 13, 17, 19, 23, 29 };
-
-    private static readonly long Thirty = 2 * 3 * 5;
-
     public static IEnumerable<long> Create()
     {
-        var naturals = NumberList.NaturalNumbers();
+        var naturals = NumberList.NaturalNumbers()
+            .Skip(1);
         var primes = new List<long>();
-
-        foreach (var prime in PrimeFactorsStartForThirty)
-        {
-            primes.Add(prime);
-            yield return prime;
-        }
 
         foreach (var natural in naturals)
         {
-            foreach (var wheelFactorizationCandidate in WheelFactorizationCandidatesForBaseThirty)
+            if (primes.Any(prime => natural.IsDivisibleBy(prime)))
             {
-                var candidate = Thirty * natural + wheelFactorizationCandidate;
-
-                if (primes.Any(prime => candidate.IsDivisibleBy(prime)))
-                {
-                    continue;
-                }
-
-                primes.Add(candidate);
-
-                yield return candidate;
+                continue;
             }
+
+            primes.Add(natural);
+
+            yield return natural;
         }
     }
 }
