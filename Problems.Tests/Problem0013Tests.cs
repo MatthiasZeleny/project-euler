d1b21eea6676d0a7dad6b742a8342a799df42a95
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-16 10:07:23 +0200

    Problem 0013: Solve dummy problem

---
+++ Problems.Tests/Problem0013Tests.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace Problems.Tests;
+
+public class Problem0013Tests : EulerProblemTestBase<Problem0013, BigInteger>
+{
+    protected override BigInteger ExampleResult => BigInteger.Parse("8348422521");
+
+    protected override BigInteger ProblemResult => BigInteger.Zero;
+}
---
+++ Problems/Problem0013.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace Problems;
+
+public class Problem0013 : IEulerProblem<BigInteger>
+{
+    public BigInteger Example() => BigInteger.Parse("8348422521");
+
+    public BigInteger Solution() => BigInteger.Zero;
+}
