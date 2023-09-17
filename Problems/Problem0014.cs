6b93937919a916a0065d6f6c782c185ec8cd4455
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 18:27:17 +0200

    Problem 0014: Solve dummy problem with sequence list

--- Problems/Problem0014.cs
+++ Problems/Problem0014.cs
@@ -2,7 +2,7 @@
 
 public class Problem0014 : IEulerProblem<int>
 {
-    public int Example() => 10;
+    public int Example() => new List<int> { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 }.Count;
 
     public int Solution() => 0;
 }
