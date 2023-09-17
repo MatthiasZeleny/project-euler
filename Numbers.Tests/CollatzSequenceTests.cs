e67148346993b334529667532782519bef57d018
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 19:40:19 +0200

    Problem 0014: Merge tests

--- Numbers.Tests/CollatzSequenceTests.cs
+++ Numbers.Tests/CollatzSequenceTests.cs
@@ -7,21 +7,11 @@ public class CollatzSequenceTests
     [TestCase(1, 1)]
     [TestCase(2, 2, 1)]
     [TestCase(3, 3, 10, 5, 16, 8, 4, 2, 1)]
+    [TestCase(13, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1)]
     public void GetSequenceStartingWith_ShouldReturnCorrectSequence(int start, params int[] expectedSequence)
     {
         var sequence = CollatzSequence.GetSequenceStartingWith(start);
 
         sequence.Should().BeEquivalentToWithStrictOrdering(expectedSequence);
-    }
-
-    [Test]
-    public void GetSequenceStartingWith_Thirteen_ShouldReturnSequenceWithTenItems()
-    {
-        var sequence = CollatzSequence.GetSequenceStartingWith(13);
-
-        sequence.Should().BeEquivalentToWithStrictOrdering(new List<int>
-        {
-            13, 40, 20, 10, 5, 16, 8, 4, 2, 1
-        });
     }
 }
