f7b62b6623cfbb94e9689820eadba89c8a1c3f4c
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 18:55:12 +0200

    Problem 0014: Add test for dummy case

---
+++ Numbers.Tests/CollatzSequenceTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+
+namespace Numbers.Tests;
+
+public class CollatzSequenceTests
+{
+    [Test]
+    public void GetSequenceStartingWith_Thirteen_ShouldReturnSequenceWithTenItems()
+    {
+        var sequence = CollatzSequence.GetSequenceStartingWith(13);
+
+        sequence.Should().BeEquivalentToWithStrictOrdering(new List<int>
+        {
+            13, 40, 20, 10, 5, 16, 8, 4, 2, 1
+        });
+    }
+}
