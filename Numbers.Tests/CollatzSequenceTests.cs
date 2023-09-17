61f997069a3691d49326f101a8aeb3bdb2d60e1d
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 19:28:28 +0200

    Problem 0014: Add tests with dummy implementation

--- Numbers.Tests/CollatzSequenceTests.cs
+++ Numbers.Tests/CollatzSequenceTests.cs
@@ -4,6 +4,16 @@ namespace Numbers.Tests;
 
 public class CollatzSequenceTests
 {
+    [TestCase(1, 1)]
+    [TestCase(2, 2, 1)]
+    [TestCase(3, 3, 10, 5, 16, 8, 4, 2, 1)]
+    public void GetSequenceStartingWith_ShouldReturnCorrectSequence(int start, params int[] expectedSequence)
+    {
+        var sequence = CollatzSequence.GetSequenceStartingWith(start);
+
+        sequence.Should().BeEquivalentToWithStrictOrdering(expectedSequence);
+    }
+
     [Test]
     public void GetSequenceStartingWith_Thirteen_ShouldReturnSequenceWithTenItems()
     {
--- Numbers/CollatzSequence.cs
+++ Numbers/CollatzSequence.cs
@@ -2,5 +2,23 @@
 
 public static class CollatzSequence
 {
-    public static List<int> GetSequenceStartingWith(int start) => new() { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 };
+    public static List<int> GetSequenceStartingWith(int start)
+    {
+        if (start == 1)
+        {
+            return new List<int> { 1 };
+        }
+
+        if (start == 2)
+        {
+            return new List<int> { 2, 1 };
+        }
+
+        if (start == 3)
+        {
+            return new List<int> { 3, 10, 5, 16, 8, 4, 2, 1 };
+        }
+
+        return new List<int> { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 };
+    }
 }
