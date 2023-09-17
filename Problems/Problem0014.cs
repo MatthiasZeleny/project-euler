e8f6450b9b8892aa8aa05c1e54890d0cd33b7302
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 19:43:13 +0200

    Problem 0014: Fix compilation error

--- Problems/Problem0014.cs
+++ Problems/Problem0014.cs
@@ -4,7 +4,7 @@ namespace Problems;
 
 public class Problem0014 : IEulerProblem<int>
 {
-    public int Example() => CollatzSequence.GetSequenceStartingWith(13).Count;
+    public int Example() => CollatzSequence.GetSequenceStartingWith(13).Count();
 
     public int Solution() => 0;
 }
