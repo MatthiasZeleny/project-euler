a43605c80c730c959a2d47b78c1a1d597bf1b710
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-09-17 18:24:55 +0200

    Problem 0014: Solve dummy problem

---
+++ Problems.Tests/Problem0014Tests.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems.Tests;
+
+public class Problem0014Tests : EulerProblemTestBase<Problem0014, int>
+{
+    protected override int ExampleResult => 10;
+
+    protected override int ProblemResult => 0;
+}
---
+++ Problems/Problem0014.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems;
+
+public class Problem0014 : IEulerProblem<int>
+{
+    public int Example() => 10;
+
+    public int Solution() => 0;
+}
