b60de84c44c21ce99ec4a0326bd392325e776549
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-21 10:24:58 +0200

    General: Add tests for Grid

---
+++ Numbers.Tests/GridTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace Numbers.Tests;
+
+public class GridTests
+{
+    [TestCase(0, 0, 0, 0)]
+    [TestCase(1, 0, 1, 0)]
+    [TestCase(0, 1, 1, 0)]
+    public void METHOD(long edgeCountWidth, long edgeCountHeight, long expectedNormalizedCountWidth,
+        long expectedNormalizedCountHeight)
+    {
+        var grid = new Grid(edgeCountWidth, edgeCountHeight);
+
+        var normalizedSize = grid.NormalizedSize;
+
+        normalizedSize.Should().Be((expectedNormalizedCountWidth, expectedNormalizedCountHeight));
+    }
+}
