77aa9ee92d5aaf2db532600b3ebf0fd855469692
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-21 10:28:43 +0200

    General: Fix test name

--- Numbers.Tests/GridTests.cs
+++ Numbers.Tests/GridTests.cs
@@ -7,7 +7,10 @@ public class GridTests
     [TestCase(0, 0, 0, 0)]
     [TestCase(1, 0, 1, 0)]
     [TestCase(0, 1, 1, 0)]
-    public void METHOD(long edgeCountWidth, long edgeCountHeight, long expectedNormalizedCountWidth,
+    public void NormalizedGridSize_ShouldReturnBiggerNumberFirst(
+        long edgeCountWidth,
+        long edgeCountHeight,
+        long expectedNormalizedCountWidth,
         long expectedNormalizedCountHeight)
     {
         var grid = new Grid(edgeCountWidth, edgeCountHeight);
