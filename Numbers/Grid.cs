c75f3dfb0afa2a5ce89f62a2334e2c895c45cf04
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-21 10:14:03 +0200

    General: Extract Grid.cs

---
+++ Numbers/Grid.cs
@@ -0,0 +1,17 @@
+﻿namespace Numbers;
+
+public class Grid
+{
+    private long EdgeCountWidth { get; }
+    private long EdgeCountHeight { get; }
+
+    public Grid(long edgeCountWidth, long edgeCountHeight)
+    {
+        EdgeCountWidth = edgeCountWidth;
+        EdgeCountHeight = edgeCountHeight;
+    }
+
+    public (long edgeCountWidth, long edgeCoundHeight) NormalizedSize => EdgeCountWidth > EdgeCountHeight
+        ? (EdgeCountWidth, EdgeCountHeight)
+        : (EdgeCountHeight, EdgeCountWidth);
+}
--- Numbers/Grids.cs
+++ Numbers/Grids.cs
@@ -48,19 +48,4 @@ public static class Grids
         }
     }
 
-    private class Grid
-    {
-        private long EdgeCountWidth { get; }
-        private long EdgeCountHeight { get; }
-
-        public Grid(long edgeCountWidth, long edgeCountHeight)
-        {
-            EdgeCountWidth = edgeCountWidth;
-            EdgeCountHeight = edgeCountHeight;
-        }
-
-        public (long edgeCountWidth, long edgeCoundHeight) NormalizedSize => EdgeCountWidth > EdgeCountHeight
-            ? (EdgeCountWidth, EdgeCountHeight)
-            : (EdgeCountHeight, EdgeCountWidth);
-    }
 }
