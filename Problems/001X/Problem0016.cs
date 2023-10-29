dd5b1542b9c40e86e43adf5d4640ab0de3a1dd1d
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-29 18:22:40 +0100

    Problem 0016: Solve dummy problem with actual logic

--- Problems/001X/Problem0016.cs
+++ Problems/001X/Problem0016.cs
@@ -1,8 +1,13 @@
-﻿namespace Problems._001X;
+﻿using Numbers;
+
+namespace Problems._001X;
 
 public class Problem0016 : IEulerProblem<long>
 {
-    public long Example() => 3 + 2 + 7 + 6 + 8;
+    public long Example() => 
+        Enumerable.Repeat(2, 15)
+            .Aggregate(1L, (product, nextFactor) => product * nextFactor)
+        .ToDigitList().Sum();
 
     public long Solution() => 0;
 }
