6563eda7ec0d0ba74123848e3cebedcf09b745c2
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-29 18:24:46 +0100

    Problem 0016: Refactoring

--- Problems/001X/Problem0016.cs
+++ Problems/001X/Problem0016.cs
@@ -4,12 +4,14 @@ namespace Problems._001X;
 
 public class Problem0016 : IEulerProblem<long>
 {
-    public long Example() =>
-        TwoToThePowerOf(15)
-            .ToDigitList()
-            .Sum();
+    public long Example() => GetDigitSumOfTwoToThePowerOf(15);
 
     public long Solution() => 0;
+
+    private static long GetDigitSumOfTwoToThePowerOf(int count) =>
+        TwoToThePowerOf(count)
+            .ToDigitList()
+            .Sum();
 
     private static long TwoToThePowerOf(int count) =>
         Enumerable
