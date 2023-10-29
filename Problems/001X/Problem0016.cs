52ae4e6a843284f9db1ab58695ff1d03048d47ed
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-29 18:24:06 +0100

    Problem 0016: Refactoring

--- Problems/001X/Problem0016.cs
+++ Problems/001X/Problem0016.cs
@@ -4,10 +4,15 @@ namespace Problems._001X;
 
 public class Problem0016 : IEulerProblem<long>
 {
-    public long Example() => 
-        Enumerable.Repeat(2, 15)
-            .Aggregate(1L, (product, nextFactor) => product * nextFactor)
-        .ToDigitList().Sum();
+    public long Example() =>
+        TwoToThePowerOf(15)
+            .ToDigitList()
+            .Sum();
 
     public long Solution() => 0;
+
+    private static long TwoToThePowerOf(int count) =>
+        Enumerable
+            .Repeat(2, count)
+            .Aggregate(1L, (product, nextFactor) => product * nextFactor);
 }
