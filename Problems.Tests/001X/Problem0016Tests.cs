4ba2c27b3b2c6e978671763439d8ffe1a172b3b0
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-10-30 13:02:09 +0100

    Problem 0016: Fix missing test value update

--- Problems.Tests/001X/Problem0016Tests.cs
+++ Problems.Tests/001X/Problem0016Tests.cs
@@ -6,5 +6,5 @@ public class Problem0016Tests : EulerProblemTestBase<Problem0016, long>
 {
     protected override long ExampleResult => 26;
 
-    protected override long ProblemResult => 0;
+    protected override long ProblemResult => 0x556;
 }
