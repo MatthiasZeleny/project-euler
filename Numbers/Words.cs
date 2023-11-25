c9ff10f4d52ec61d9af0d42518d46870d156fb06
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-11-25 06:00:44 +0100

    Fix indentation

--- Numbers/Words.cs
+++ Numbers/Words.cs
@@ -2,71 +2,71 @@
 
 public static class Words
 {
-	public static string ToWord(long number) =>
-		number switch
-		{
-			< 10 => CreateSingleDigit(number),
-			_ => CreateDoubleDigit(number),
-		};
+    public static string ToWord(long number) =>
+        number switch
+        {
+            < 10 => CreateSingleDigit(number),
+            _ => CreateDoubleDigit(number),
+        };
 
-	private static string CreateSingleDigit(long number) =>
-		number switch
-		{
-			1 => "one",
-			2 => "two",
-			3 => "three",
-			4 => "four",
-			5 => "five",
-			6 => "six",
-			7 => "seven",
-			8 => "eight",
-			9 => "nine",
-			_ => ThrowException(number)
-		};
+    private static string CreateSingleDigit(long number) =>
+        number switch
+        {
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            _ => ThrowException(number)
+        };
 
-	private static string CreateDoubleDigit(long number) =>
-		number switch
-		{
-			< 20 => CreateTenToNineTeen(number),
-			< 30 => "twenty" + CreateSingleDigitAsSuffix(number),
-			< 40 => "thirty" + CreateSingleDigitAsSuffix(number),
-			< 50 => "forty" + CreateSingleDigitAsSuffix(number),
-			< 60 => "fifty" + CreateSingleDigitAsSuffix(number),
-			< 70 => "sixty" + CreateSingleDigitAsSuffix(number),
-			< 80 => "seventy" + CreateSingleDigitAsSuffix(number),
-			< 90 => "eighty" + CreateSingleDigitAsSuffix(number),
-			< 100 => "ninety" + CreateSingleDigitAsSuffix(number),
-			_ => ThrowException(number)
-		};
+    private static string CreateDoubleDigit(long number) =>
+        number switch
+        {
+            < 20 => CreateTenToNineTeen(number),
+            < 30 => "twenty" + CreateSingleDigitAsSuffix(number),
+            < 40 => "thirty" + CreateSingleDigitAsSuffix(number),
+            < 50 => "forty" + CreateSingleDigitAsSuffix(number),
+            < 60 => "fifty" + CreateSingleDigitAsSuffix(number),
+            < 70 => "sixty" + CreateSingleDigitAsSuffix(number),
+            < 80 => "seventy" + CreateSingleDigitAsSuffix(number),
+            < 90 => "eighty" + CreateSingleDigitAsSuffix(number),
+            < 100 => "ninety" + CreateSingleDigitAsSuffix(number),
+            _ => ThrowException(number)
+        };
 
 
-	private static string CreateSingleDigitAsSuffix(long number)
-	{
-		var singleDigit = number % 10;
+    private static string CreateSingleDigitAsSuffix(long number)
+    {
+        var singleDigit = number % 10;
 
-		return singleDigit switch
-		{
-			0 => "",
-			_ => "-" + CreateSingleDigit(singleDigit)
-		};
-	}
+        return singleDigit switch
+        {
+            0 => "",
+            _ => "-" + CreateSingleDigit(singleDigit)
+        };
+    }
 
-	private static string CreateTenToNineTeen(long number) =>
-		number switch
-		{
-			10 => "ten",
-			11 => "eleven",
-			12 => "twelve",
-			13 => "thirteen",
-			14 => "fourteen",
-			15 => "fifteen",
-			16 => "sixteen",
-			17 => "seventeen",
-			18 => "eighteen",
-			19 => "nineteen",
-			_ => ThrowException(number)
-		};
+    private static string CreateTenToNineTeen(long number) =>
+        number switch
+        {
+            10 => "ten",
+            11 => "eleven",
+            12 => "twelve",
+            13 => "thirteen",
+            14 => "fourteen",
+            15 => "fifteen",
+            16 => "sixteen",
+            17 => "seventeen",
+            18 => "eighteen",
+            19 => "nineteen",
+            _ => ThrowException(number)
+        };
 
-	private static string ThrowException(long number) =>
-		throw new ArgumentException($"Number {number} cannot be handled.", nameof(number));
+    private static string ThrowException(long number) =>
+        throw new ArgumentException($"Number {number} cannot be handled.", nameof(number));
 }
