54e22e156dc468c37ff9ac3907349d9a9fbd1c4d
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-12-17 08:59:12 +0100

    Refactoring

--- Problems/001X/Problem0017.cs
+++ Problems/001X/Problem0017.cs
@@ -4,12 +4,14 @@ namespace Problems._001X;
 
 public class Problem0017 : IEulerProblem<long>
 {
-    public long Example() =>
-        CreateWordListUpUntil(5).Select(word => word.Length).Sum();
+    public long Example() => CountLettersUpTo(5);
 
     public long Solution() => 0;
 
-    private static IEnumerable<string> CreateWordListUpUntil(int highestNumber) =>
+    private static int CountLettersUpTo(int highestNumber) =>
+        CreateWordListUpTo(highestNumber).Select(word => word.Length).Sum();
+
+    private static IEnumerable<string> CreateWordListUpTo(int highestNumber) =>
         NumberList
             .NumbersUpTo(highestNumber)
             .Select(Words.ToWord);
