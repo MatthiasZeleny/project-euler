058f4efcd0c21375b919b067184907fad25a1457
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2023-12-26 09:33:54 +0100

    Problem 0018: Solve dummy problem with actual code

--- Problems/001X/Problem0018.cs
+++ Problems/001X/Problem0018.cs
@@ -1,8 +1,10 @@
-﻿namespace Problems._001X;
+﻿using Numbers;
+
+namespace Problems._001X;
 
 public class Problem0018 : IEulerProblem<long>
 {
-    public long Example() => 3 + 7 + 4 + 9;
+    public long Example() => Triangle.FromString("3\n7 4\n2 4 6\n8 5 9 3").ComputeBiggestPath();
 
     public long Solution() => 0;
 }
