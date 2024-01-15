53ef929d8c9bd9a21b84a953233d8ff43543ae5a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-01-15 19:45:00 +0100

    Problem 0019: Solve dummy problem correctly

--- Problems/001X/Problem0019.cs
+++ Problems/001X/Problem0019.cs
@@ -1,9 +1,16 @@
-﻿namespace Problems._001X;
+﻿using Numbers;
+
+namespace Problems._001X;
 
 public class Problem0019 : IEulerProblem<long>
 {
-    public long Example() => 0 + 0 + 0 + 1 + 0 + 0 +
-                             1 + 0 + 0 + 0 + 0 + 0;
+    public long Example() => GetCountOfFirstOfMonthSundays(1900);
 
     public long Solution() => 0;
+
+    private static int GetCountOfFirstOfMonthSundays(int lastYear) =>
+        Date
+            .GetDaysStartingFromFirstDayOf1900()
+            .TakeWhile(date => date.Year <= lastYear)
+            .Count(date => date.DayOfWeek == DayOfWeek.Sunday && date.DayInMonth == 1);
 }
