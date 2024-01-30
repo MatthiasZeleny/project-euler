848087296e0e97c73734c961a02e4088f394d9f7
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-01-30 18:33:41 +0100

    Problem 0020: Refactoring

--- Problems/002X/Problem0020.cs
+++ Problems/002X/Problem0020.cs
@@ -5,8 +5,14 @@ namespace Problems._002X;
 
 public class Problem0020 : IEulerProblem<long>
 {
-    public long Example() => Enumerable.Range(1, 10 - 1).Select(number => new BigInteger(number))
-        .Aggregate((product, factor) => product * factor).ToDigitList().Sum();
+    public long Example() => SumOfFactorialDigits(10);
 
     public long Solution() => 0;
+
+    private static long SumOfFactorialDigits(int largestFactor) =>
+        Enumerable.Range(1, largestFactor - 1)
+            .Select(number => new BigInteger(number))
+            .Aggregate((product, factor) => product * factor)
+            .ToDigitList()
+            .Sum();
 }
