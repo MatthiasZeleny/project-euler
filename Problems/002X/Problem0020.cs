f2da51fc8257e3f50d5a21afd7b370f01da7ee65
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-01-30 18:32:33 +0100

    Problem 0020: Solve dummy correctly

--- Problems/002X/Problem0020.cs
+++ Problems/002X/Problem0020.cs
@@ -1,8 +1,12 @@
-﻿namespace Problems._002X;
+﻿using System.Numerics;
+using Numbers;
+
+namespace Problems._002X;
 
 public class Problem0020 : IEulerProblem<long>
 {
-    public long Example() => 3 + 6 + 2 + 8 + 8 + 0 + 0;
+    public long Example() => Enumerable.Range(1, 10 - 1).Select(number => new BigInteger(number))
+        .Aggregate((product, factor) => product * factor).ToDigitList().Sum();
 
     public long Solution() => 0;
 }
