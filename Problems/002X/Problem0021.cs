07c9d1d42bcbb9dd519beb0a8d2ca2ae41b925d9
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-02-10 18:14:20 +0100

    Problem 0021: Solve dummy problem with more explanation

--- Problems/002X/Problem0021.cs
+++ Problems/002X/Problem0021.cs
@@ -1,8 +1,13 @@
-﻿namespace Problems._002X;
+﻿using Numbers;
+
+namespace Problems._002X;
 
 public class Problem0021 : IEulerProblem<long>
 {
-    public long Example() => 220 + 280;
+    public long Example() => GetSumOfAmicableNumbersBelow(1000);
 
     public long Solution() => 0;
+
+    private static long GetSumOfAmicableNumbersBelow(long below) =>
+        NumberList.Below(below).Where(number => number is 220 or 280).Sum();
 }
