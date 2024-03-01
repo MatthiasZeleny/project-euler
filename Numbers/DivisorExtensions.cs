aeaf1dde1d9edb8876d0ad8c4b0d96e828b259a5
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-03-01 18:07:48 +0100

    Problem 0021: Implement GetProperDivisors fully

--- Numbers/DivisorExtensions.cs
+++ Numbers/DivisorExtensions.cs
@@ -2,33 +2,8 @@
 
 public static class DivisorExtensions
 {
-    public static IReadOnlyCollection<long> GetProperDivisors(this long number)
-    {
-        return number switch
-        {
-            220 =>
-            [
-                1,
-                2,
-                4,
-                5,
-                10,
-                11,
-                20,
-                22,
-                44,
-                55,
-                110
-            ],
-            284 =>
-            [
-                1,
-                2,
-                4,
-                71,
-                142
-            ],
-            _ => new List<long>()
-        };
-    }
+    public static IReadOnlyCollection<long> GetProperDivisors(this long number) =>
+        NumberList.Below(number)
+            .Where(divisorCandidate => number.IsDivisibleBy(divisorCandidate))
+            .ToList();
 }
