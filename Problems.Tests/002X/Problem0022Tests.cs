a83d9d6d05f07518a7e8b96437892535f67a618a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-03-03 08:19:13 +0100

    Problem 0022: Dummy solution to dummy problem

---
+++ Problems.Tests/002X/Problem0022Tests.cs
@@ -0,0 +1,10 @@
+﻿using Problems._002X;
+
+namespace Problems.Tests._002X;
+
+public class Problem0022Tests : EulerProblemTestBase<Problem0022, long>
+{
+    protected override long ExampleResult => 49714;
+
+    protected override long ProblemResult => 0;
+}
---
+++ Problems/002X/Problem0022.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._002X;
+
+public class Problem0022 : IEulerProblem<long>
+{
+    public long Example() => (3 + 15 + 12 + 9 + 14) * 938;
+
+    public long Solution() => 0;
+}
