3182b1f7ea01a74b3053036b1e505c6ff3f1311d
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-03-06 19:02:40 +0100

    Problem 0022: slightly better solution to dummy problem

--- Problems/002X/Problem0022.cs
+++ Problems/002X/Problem0022.cs
@@ -2,7 +2,13 @@
 
 public class Problem0022 : IEulerProblem<long>
 {
-    public long Example() => (3 + 15 + 12 + 9 + 14) * 938;
+    public long Example() => WordToSumOfPositionInAlphabet("COLIN") * 938L;
 
     public long Solution() => 0;
+
+    private static long WordToSumOfPositionInAlphabet(string word) => 
+        word
+            .ToList()
+            .Select(character => (long)character - 'A' + 1)
+            .Sum();
 }
