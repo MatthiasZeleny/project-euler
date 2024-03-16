15013df33b9eff47410e66c30f1c102e91b28611
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-03-16 12:11:58 +0100

    Problem 0022: Try and fail solving problem

--- Problems/002X/Problem0022.cs
+++ Problems/002X/Problem0022.cs
@@ -1,10 +1,37 @@
-﻿namespace Problems._002X;
+﻿using Utils;
+
+namespace Problems._002X;
 
 public class Problem0022 : IEulerProblem<long>
 {
-    public long Example() => WordToSumOfPositionInAlphabet("COLIN") * 938L;
+    public long Example() => ComputeSumOfSortedWordListMultipliedByPosition(name => name == "COLIN");
 
-    public long Solution() => 0;
+    public long Solution() => ComputeSumOfSortedWordListMultipliedByPosition(_ => true);
+
+    private static long ComputeSumOfSortedWordListMultipliedByPosition(Func<string, bool> filter) =>
+        GetNames()
+            .Then(names => names.Order())
+            .Then(names => names.Select((name, index) => (Name: name, Position: index + 1)))
+            .Where(tuple => filter(tuple.Name))
+            .Select(
+                tuple =>
+                {
+                    var wordValueTimesPosition = GetWordValueTimesPosition(tuple.Name, tuple.Position);
+                    Console.WriteLine(tuple + "=" + wordValueTimesPosition);
+
+                    return wordValueTimesPosition;
+                })
+            .Sum();
+
+    private static IEnumerable<string> GetNames()
+    {
+        return File.ReadAllText(Path.Join("002X", "0022_Names.txt"))
+            .Split(',')
+            .Select(nameWithQuotes => nameWithQuotes.Trim('"'));
+    }
+
+    private static long GetWordValueTimesPosition(string name, long position) =>
+        WordToSumOfPositionInAlphabet(name) * position;
 
     private static long WordToSumOfPositionInAlphabet(string word) =>
         word
@@ -12,5 +39,10 @@ public class Problem0022 : IEulerProblem<long>
             .Select(CharacterToNumber)
             .Sum();
 
-    private static long CharacterToNumber(char character) => (long)character - 'A' + 1;
+    private static long CharacterToNumber(char character) =>
+        character switch
+        {
+            >= 'A' and <= 'Z' => (long)character - 'A' + 1,
+            _ => throw new ArgumentException($"Character not valid: {{{character}}}")
+        };
 }
