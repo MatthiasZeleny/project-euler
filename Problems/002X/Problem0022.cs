cf98825b54cf8264506a19347603609788f12c94
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-03-23 08:08:51 +0100

    Problem 0022: Refactor and remove logging

--- Problems/002X/Problem0022.cs
+++ Problems/002X/Problem0022.cs
@@ -13,14 +13,7 @@ public class Problem0022 : IEulerProblem<long>
             .Then(names => names.Order())
             .Then(names => names.Select((name, index) => (Name: name, Position: index + 1)))
             .Where(tuple => filter(tuple.Name))
-            .Select(
-                tuple =>
-                {
-                    var wordValueTimesPosition = GetWordValueTimesPosition(tuple.Name, tuple.Position);
-                    Console.WriteLine(tuple + "=" + wordValueTimesPosition);
-
-                    return wordValueTimesPosition;
-                })
+            .Select(GetWordValueTimesPosition)
             .Sum();
 
     private static IEnumerable<string> GetNames()
@@ -30,8 +23,8 @@ public class Problem0022 : IEulerProblem<long>
             .Select(nameWithQuotes => nameWithQuotes.Trim('"'));
     }
 
-    private static long GetWordValueTimesPosition(string name, long position) =>
-        WordToSumOfPositionInAlphabet(name) * position;
+    private static long GetWordValueTimesPosition((string Name, int Position) tuple) =>
+        WordToSumOfPositionInAlphabet(tuple.Name) * tuple.Position;
 
     private static long WordToSumOfPositionInAlphabet(string word) =>
         word
