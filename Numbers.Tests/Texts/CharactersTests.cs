82cb8963ab2402e042f96967d1022aadb4705e8e
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-04-02 18:15:05 +0200

    General: Add tests

---
+++ Numbers.Tests/Texts/CharactersTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Numbers.Texts;
+
+namespace Numbers.Tests.Texts;
+
+public class CharactersTests
+{
+
+    [TestCase('A', 1)]
+    [TestCase('Z', 26)]
+    public void CharacterToNumber(char character, int expected)
+    {
+        var number = Characters.CharacterToNumber(character);
+
+        number.Should().Be(expected);
+    }
+}
