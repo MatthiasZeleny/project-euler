93f1e089843dc362d1c697fe032d00f7b40da1f3
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-04-13 09:16:19 +0200

    Problem 0023: Add AbundantNumberExtensions

---
+++ Numbers.Tests/SpecialNumbers/AbundantNumberExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Numbers.SpecialNumbers;
+
+namespace Numbers.Tests.SpecialNumbers;
+
+public class AbundantNumberExtensionsTests
+{
+
+    [TestCase(1,false)]
+    [TestCase(2,false)]
+    [TestCase(12,true)]
+    public void IsAbundantNumber_ShouldReturnCorrectResult(long number, bool expected)
+    {
+        var isAbundantNumber = number.IsAbundantNumber();
+
+        isAbundantNumber.Should().Be(expected);
+    }
+}
---
+++ Numbers/SpecialNumbers/AbundantNumberExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Numbers.BasicMath;
+
+namespace Numbers.SpecialNumbers;
+
+public static class AbundantNumberExtensions
+{
+    public static bool IsAbundantNumber(this long number)
+    {
+        var sumOfProperDivisors = number.GetProperDivisors().Sum();
+
+        return number < sumOfProperDivisors;
+    }
+}
