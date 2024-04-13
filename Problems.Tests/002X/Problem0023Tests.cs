97f27d6e62a322f1e5380c917263543d36822af2
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-04-13 09:03:24 +0200

    Problem 0023: Solve example with dummy solution

---
+++ Problems.Tests/002X/Problem0023Tests.cs
@@ -0,0 +1,10 @@
+﻿using Problems._002X;
+
+namespace Problems.Tests._002X;
+
+public class Problem0023Tests : EulerProblemTestBase<Problem0023, long>
+{
+    protected override long ExampleResult => 276;
+
+    protected override long ProblemResult => 0;
+}
---
+++ Problems/002X/Problem0023.cs
@@ -0,0 +1,10 @@
+﻿using Numbers.BasicMath;
+
+namespace Problems._002X;
+
+public class Problem0023 : IEulerProblem<long>
+{
+    public long Example() => NumberList.NumbersUpTo(24-1).Sum();
+
+    public long Solution() => 0;
+}
