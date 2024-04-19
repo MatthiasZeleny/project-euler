e1b214f7d8e7f35f1f5f0ee3507b7bc076a52545
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-04-19 18:30:49 +0200

    Problem 0023: Solve example

--- Problems/002X/Problem0023.cs
+++ Problems/002X/Problem0023.cs
@@ -1,4 +1,5 @@
 ﻿using Numbers.BasicMath;
+using Numbers.SpecialNumbers;
 
 namespace Problems._002X;
 
@@ -6,9 +7,19 @@ public class Problem0023 : IEulerProblem<long>
 {
     private const int SmallestNumberWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers = 24;
 
-    public long Example() => SumOfAllWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers(SmallestNumberWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers);
+    public long Example() =>
+        SumOfAllWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers(
+            SmallestNumberWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers);
 
     public long Solution() => 0;
 
-    private static long SumOfAllWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers(int maximumNumber) => NumberList.NumbersUpTo(maximumNumber - 1).Sum();
+    private static long SumOfAllWhichCannotBeWrittenAsTheSumOfTwoAbundantNumbers(int maximumNumber)
+    {
+        var numbersUpTo = NumberList.NumbersUpTo(maximumNumber).ToList();
+
+        var abundantNumbers = numbersUpTo.Where(number => number.IsAbundantNumber()).ToList();
+        var possibleSums = abundantNumbers.Zip(abundantNumbers, (first, second) => first + second).Distinct().ToList();
+
+        return numbersUpTo.Where(number => possibleSums.Contains(number) is false).Sum();
+    }
 }
