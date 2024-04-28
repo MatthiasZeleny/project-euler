d66e08a1192aa431ef50bc748b62a7d821f17c17
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-04-28 08:07:44 +0200

    Problem 0024: Refactoring

--- Problems/002X/Problem0024.cs
+++ Problems/002X/Problem0024.cs
@@ -2,7 +2,13 @@
 
 public class Problem0024 : IEulerProblem<long>
 {
-    public long Example() => new List<long> { 012, 021, 102, 120, 201, 210 }[4 - 1];
+    public long Example() => GetNthElementOfLexicographicOrderOfDigitsUpTill(4, 2);
 
     public long Solution() => 0;
+
+    private static long GetNthElementOfLexicographicOrderOfDigitsUpTill(int position, int highestDigit) =>
+        GetLexicographicOrderedUpTo(highestDigit)[position - 1];
+
+    private static List<long> GetLexicographicOrderedUpTo(int highestDigit) =>
+        new() { 012, 021, 102, 120, 201, 210 };
 }
