1c4220ac6b8150d689ffcf688b2a12f62d02037b
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-05-26 07:51:43 +0200

    Problem 0025: Refactoring

--- Problems/002X/Problem0025.cs
+++ Problems/002X/Problem0025.cs
@@ -6,9 +6,12 @@ namespace Problems._002X;
 
 public class Problem0025 : IEulerProblem<BigInteger>
 {
-    public BigInteger Example() => Fibonacci.GetAllLessOrEqualStartingWithOneOneBigInteger()
-        .Select((number, index) => (Number: number, EntryNumber: index + 1))
-        .First(tuple => tuple.Number.ToDigitList().Count() >= 3).EntryNumber;
+    public BigInteger Example() => GetPositionOfFirstFibonacciNumberWithNDigits(3);
 
     public BigInteger Solution() => 0;
+
+    private static BigInteger GetPositionOfFirstFibonacciNumberWithNDigits(int numberOfDigits) =>
+        Fibonacci.GetAllLessOrEqualStartingWithOneOneBigInteger()
+            .Select((number, index) => (Number: number, EntryNumber: index + 1))
+            .First(tuple => tuple.Number.ToDigitList().Count() >= numberOfDigits).EntryNumber;
 }
