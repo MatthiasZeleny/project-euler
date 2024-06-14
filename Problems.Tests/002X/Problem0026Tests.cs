b7c49e7052b60ce43f2544a8a0f04883ec27a5f3
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-06-14 19:04:50 +0200

    Problem 0026: Swap productive and test code

--- Problems.Tests/002X/Problem0026Tests.cs
+++ Problems.Tests/002X/Problem0026Tests.cs
@@ -4,6 +4,6 @@ namespace Problems.Tests._002X;
 
 public class Problem0026Tests : EulerProblemTestBase<Problem0026, int>
 {
-    protected override int ExampleResult => new List<int> { 0, 1, 0, 0, 1, 6, 0, 1, 0 }.Max();
+    protected override int ExampleResult => 6;
     protected override int ProblemResult => 0;
 }
--- Problems/002X/Problem0026.cs
+++ Problems/002X/Problem0026.cs
@@ -2,7 +2,7 @@
 
 public class Problem0026 : IEulerProblem<int>
 {
-    public int Example() => 6;
+    public int Example() => new List<int> { 0, 1, 0, 0, 1, 6, 0, 1, 0 }.Max();
 
     public int Solution() => 0;
 }
