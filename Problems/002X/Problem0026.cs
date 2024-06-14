7b73f827622f6aa41c9fc0851a927ae0a01839ce
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-06-14 19:23:39 +0200

    Problem 0026: Fix extraction

--- Problems/002X/Problem0026.cs
+++ Problems/002X/Problem0026.cs
@@ -4,8 +4,10 @@ namespace Problems._002X;
 
 public class Problem0026 : IEulerProblem<long>
 {
-    public long Example() => NumberList.NumbersUpTo(10)
-        .Select(DenominatorExtensions.GetLengthOfRecurringCycleForOneDividedBy).Max();
+    public long Example() =>
+        NumberList.NumbersUpTo(10)
+            .Skip(1)
+            .Select(DenominatorExtensions.GetLengthOfRecurringCycleForOneDividedBy).Max();
 
     public long Solution() => 0;
 }
