cad575b1d73de2e5e5abcc36436deb33d3130282
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-06-23 06:22:09 +0200

    Problem 0026: Extract recurring cycle method

--- Numbers/BasicMath/DenominatorExtensions.cs
+++ Numbers/BasicMath/DenominatorExtensions.cs
@@ -3,11 +3,16 @@
 public static class DenominatorExtensions
 {
     public static long GetLengthOfRecurringCycleForOneDividedBy(this long number) =>
+        GetRecurringCycleForOneDividedBy(number).Count;
+
+    private static List<int> GetRecurringCycleForOneDividedBy(long number) =>
         number switch
         {
-            2 or 4 or 5 or 8 or 10 => 0,
-            3 or 6 or 9 => 1,
-            7 => 6,
-            _ => throw new ArgumentException($"Unhandled case: {number}")
+            2 or 4 or 5 or 8 or 10 => [],
+            3 => [3],
+            6 => [6],
+            7 => [1, 4, 2, 8, 5, 7],
+            9 => [1],
+            _ => throw new ArgumentOutOfRangeException(nameof(number), number, null)
         };
 }
