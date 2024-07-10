0e760d9657d67fee81c876dd8a3533a65e6bf2f6
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-07-10 18:48:32 +0200

    Problem 0026: Refactoring

--- Numbers/BasicMath/DenominatorExtensions.cs
+++ Numbers/BasicMath/DenominatorExtensions.cs
@@ -1,18 +1,26 @@
-﻿namespace Numbers.BasicMath;
+﻿using Numbers.SpecialNumbers.Primes;
+
+namespace Numbers.BasicMath;
 
 public static class DenominatorExtensions
 {
     public static long GetLengthOfRecurringCycleForOneDividedBy(this long number) =>
         GetRecurringCycleForOneDividedBy(number).Count;
 
-    private static List<int> GetRecurringCycleForOneDividedBy(long number) =>
-        number switch
+    private static List<int> GetRecurringCycleForOneDividedBy(long number)
+    {
+        if (PrimeFactorRepresentation.For(number).AsList().All(primeFactor => primeFactor is 2 or 5))
         {
-            2 or 4 or 5 or 8 or 10 => [],
+            return [];
+        }
+
+        return number switch
+        {
             3 => [3],
             6 => [6],
             7 => [1, 4, 2, 8, 5, 7],
             9 => [1],
             _ => throw new ArgumentOutOfRangeException(nameof(number), number, null)
         };
+    }
 }
