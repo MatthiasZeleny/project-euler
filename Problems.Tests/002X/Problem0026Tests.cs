94dc97a08651081c460fdd4c3f261730a50afa9b
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-07-15 09:30:27 +0200

    Problem 0026: Fix example problem after reading correctly

--- Problems.Tests/002X/Problem0026Tests.cs
+++ Problems.Tests/002X/Problem0026Tests.cs
@@ -4,6 +4,6 @@ namespace Problems.Tests._002X;
 
 public class Problem0026Tests : EulerProblemTestBase<Problem0026, long>
 {
-    protected override long ExampleResult => 6;
+    protected override long ExampleResult => 7;
     protected override long ProblemResult => 0;
 }
