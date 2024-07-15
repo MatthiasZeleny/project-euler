799f215a1bd25c473974b5e777a6fa7f086f7f68
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-07-15 09:13:57 +0200

    Problem 0026: Refactoring

--- Problems/002X/Problem0026.cs
+++ Problems/002X/Problem0026.cs
@@ -4,10 +4,13 @@ namespace Problems._002X;
 
 public class Problem0026 : IEulerProblem<long>
 {
-    public long Example() =>
-        NumberList.NumbersUpTo(10)
+    public long Example() => GetLengthOfLongestRecurringCycle(10);
+
+    public long Solution() => 0;
+
+    private static long GetLengthOfLongestRecurringCycle(int maxDivisor) =>
+        NumberList.NumbersUpTo(maxDivisor)
             .Skip(1)
             .Select(DenominatorExtensions.GetLengthOfRecurringCycleForOneDividedBy).Max();
 
-    public long Solution() => 0;
 }
