67c9ccd5bb77eb66fbc4103c04e8f595e315d01a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-08-10 19:07:33 +0200

    Problem 0027: Create CachingEnumerator

---
+++ Numbers.Tests/DataUtils/CachingEnumeratorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Numbers.DataUtils;
+
+namespace Numbers.Tests.DataUtils;
+
+public class CachingEnumeratorTests
+{
+    [Test]
+    public void GetEnumerable_Once_ShouldStartWithFirstElement()
+    {
+        var source = new List<long>
+        {
+            1
+        };
+
+        var reusableEnumeratorList = new CachingEnumerator(source);
+
+        reusableEnumeratorList.GetElements().First().Should().Be(1);
+    }
+}
---
+++ Numbers/DataUtils/CachingEnumerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Numbers.DataUtils;
+
+public class CachingEnumerator(IEnumerable<long> source)
+{
+
+    public IEnumerable<long> GetElements()
+    {
+        return source;
+    }
+}
