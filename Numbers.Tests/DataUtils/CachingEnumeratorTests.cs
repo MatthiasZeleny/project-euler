acc8b8274591cd7487461832a80b1042d5c83325
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-08-25 09:14:29 +0200

    Problem 0027: Cache enumerator values

--- Numbers.Tests/DataUtils/CachingEnumeratorTests.cs
+++ Numbers.Tests/DataUtils/CachingEnumeratorTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections;
+using FluentAssertions;
 using Numbers.DataUtils;
 
 namespace Numbers.Tests.DataUtils;
@@ -43,5 +44,39 @@ public class CachingEnumeratorTests
 
         reusableEnumeratorList.GetElements().First().Should().Be(1);
         reusableEnumeratorList.GetElements().First().Should().Be(1);
+    }
+
+    [Test]
+    public void GetEnumerable_TakeOneAndThenAgain_ShouldCallEnumeratorOnlyOnce()
+    {
+        var source = new CountingEnumerableMock(
+            new List<long>
+            {
+                1
+            });
+
+        var reusableEnumeratorList = new CachingEnumerator(source);
+
+        reusableEnumeratorList.GetElements().First().Should().Be(1);
+        source.EnumeratorCallsCount.Should().Be(1);
+        reusableEnumeratorList.GetElements().First().Should().Be(1);
+
+        source.EnumeratorCallsCount.Should().Be(1);
+    }
+
+    private class CountingEnumerableMock(List<long> source) : IEnumerable<long>
+    {
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            EnumeratorCallsCount++;
+
+            return source.GetEnumerator();
+        }
+
+        public int EnumeratorCallsCount { get; private set; }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
     }
 }
--- Numbers/DataUtils/CachingEnumerator.cs
+++ Numbers/DataUtils/CachingEnumerator.cs
@@ -2,9 +2,23 @@
 
 public class CachingEnumerator(IEnumerable<long> source)
 {
+    private readonly IEnumerator<long> _enumerator = source.GetEnumerator();
+    private readonly List<long> _list = new List<long>();
 
     public IEnumerable<long> GetElements()
     {
-        return source;
+        foreach (var cachedElement in _list)
+        {
+            yield return cachedElement;
+        }
+
+        while (_enumerator.MoveNext())
+        {
+            var next = _enumerator.Current;
+
+            _list.Add(next);
+
+            yield return next;
+        }
     }
 }
