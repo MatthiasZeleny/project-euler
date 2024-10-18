3205f12695f7e196281ce88805745729af9a4d32
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-10-18 17:58:25 +0200

    Problem 0028: Dummy solve example

---
+++ Problems.Tests/002X/Problem0028Tests.cs
@@ -0,0 +1,9 @@
+﻿using Problems._002X;
+
+namespace Problems.Tests._002X;
+
+public class Problem0028Tests : EulerProblemTestBase<Problem0028, long>
+{
+    protected override long ExampleResult => 101;
+    protected override long ProblemResult => 0;
+}
---
+++ Problems/002X/Problem0028.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._002X;
+
+public class Problem0028 : IEulerProblem<long>
+{
+    public long Example() => 1 + 3 + 5 + 7 + 9 + 13 + 17 + 21 + 25;
+
+    public long Solution() => 0;
+}
