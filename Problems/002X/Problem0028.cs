6fbf06ac3d6b22f6f3aafa78b9a97c0de630b494
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-11-01 09:07:14 +0100

    Problem 0028: Refactoring

--- Problems/002X/Problem0028.cs
+++ Problems/002X/Problem0028.cs
@@ -2,7 +2,9 @@
 
 public class Problem0028 : IEulerProblem<long>
 {
-    public long Example() => 1 + 3 + 5 + 7 + 9 + 13 + 17 + 21 + 25;
+    public long Example() => GetSumOfCornerValuesFor5X5();
+
+    private static int GetSumOfCornerValuesFor5X5() => 1 + 3 + 5 + 7 + 9 + 13 + 17 + 21 + 25;
 
     public long Solution() => 0;
 }
