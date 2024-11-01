414ef83278039fa6c57e95e1b5448cd94fa77eb5
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2024-11-01 09:41:23 +0100

    Problem 0028: Refactoring

--- Problems/002X/Problem0028.cs
+++ Problems/002X/Problem0028.cs
@@ -2,17 +2,20 @@
 
 public class Problem0028 : IEulerProblem<long>
 {
+    private const int FourDirections = 4;
+
     public long Example() => GetSumOfCornerValuesFor5X5();
 
     private static int GetSumOfCornerValuesFor5X5() => CreateListOfCornerValuesFor5X5().Sum();
 
     private static IEnumerable<int> CreateListOfCornerValuesFor5X5()
     {
-        List<int> skips = [2, 2, 2, 2, 4, 4, 4, 4];
+        var skips = ((List<int>) [2, 4]).SelectMany(skipSize => Enumerable.Repeat(skipSize, FourDirections));
 
         var counter = 1;
 
         yield return counter;
+
         foreach (var skip in skips)
         {
             counter += skip;
