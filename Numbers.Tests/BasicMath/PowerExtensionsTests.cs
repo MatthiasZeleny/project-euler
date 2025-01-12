14e9d1b804eea249f315b9bf3ba43c1c253ece96
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-01-12 07:53:29 +0100

    Problem 0029: Add tests

---
+++ Numbers.Tests/BasicMath/PowerExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Numbers.BasicMath;
+
+namespace Numbers.Tests.BasicMath;
+
+public class PowerExtensionsTests
+{
+
+    [TestCase(1, 1, 1)]
+    [TestCase(1, 0, 1)]
+    [TestCase(0, 1, 0)]
+    [TestCase(0, 0, 1)]
+    [TestCase(1, 2, 1)]
+    [TestCase(2, 1, 2)]
+    [TestCase(2, 3, 8)]
+    public void ToThePowerOf_ShouldReturnExpectedResult(int baseNumber, int exponent, int expectedResult)
+    {
+        var result = baseNumber.ToThePowerOf(exponent);
+
+        result.Should().Be(expectedResult);
+    }
+}
