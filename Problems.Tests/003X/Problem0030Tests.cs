27693b8d324391a5ac8d6195e80950747ac776f5
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-01-25 06:33:31 +0100

    Problem 0030: Add example

---
+++ Problems.Tests/003X/Problem0030Tests.cs
@@ -0,0 +1,9 @@
+﻿using Problems._003X;
+
+namespace Problems.Tests._003X;
+
+public class Problem0030Tests : EulerProblemTestBase<Problem0030, int>
+{
+    protected override int ExampleResult => 19316;
+    protected override int ProblemResult => 0x0;
+}
---
+++ Problems/003X/Problem0030.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._003X;
+
+public class Problem0030 : IEulerProblem<int>
+{
+    public int Example() => 19316;
+
+    public int Solution() => 0;
+}
