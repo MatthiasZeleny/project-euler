0a97d1a8b77ebe003551ecf703a3da64b53a0d2f
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-02-15 09:22:49 +0100

    Problem 0030: Compute threshold for search depth

--- Problems/003X/Problem0030.cs
+++ Problems/003X/Problem0030.cs
@@ -9,10 +9,21 @@ public class Problem0030 : IEulerProblem<BigInteger>
     {
         var exponent = 4;
 
-        return NumberList.NaturalNumbersUpTo(9999)
+        var numbersOfDigitsNeeded = NumberList.NaturalNumbers().First(
+            numberOfDigits => numberOfDigits * 9.ToThePowerOf(exponent) <
+                              NNinesAsNumber(numberOfDigits));
+
+        var maxValue = NNinesAsNumber(numbersOfDigitsNeeded);
+
+        return NumberList.NaturalNumbersUpTo(maxValue)
             .Skip(1)
             .Where(IsSumOfNPowerOfDigits(exponent))
             .Sum();
+    }
+
+    private static int NNinesAsNumber(long numberOfDigits)
+    {
+        return Enumerable.Repeat(9, (int)numberOfDigits).Aggregate(0, (sum, digit) => sum * 10 + digit);
     }
 
     public BigInteger Solution() => 0;
