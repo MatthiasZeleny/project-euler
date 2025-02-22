aa55868eba2967d58e84735a04246b13021640e3
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-02-22 09:44:45 +0100

    Problem 0030: Refactoring

--- Problems/003X/Problem0030.cs
+++ Problems/003X/Problem0030.cs
@@ -5,10 +5,12 @@ namespace Problems._003X;
 
 public class Problem0030 : IEulerProblem<BigInteger>
 {
-    public BigInteger Example()
-    {
-        var exponent = 4;
+    public BigInteger Example() => ComputeSumOfAllNumberWhichAreTheSumOfTheirDigitsToThePowerOfN(4);
 
+    public BigInteger Solution() => 0;
+
+    private static BigInteger ComputeSumOfAllNumberWhichAreTheSumOfTheirDigitsToThePowerOfN(int exponent)
+    {
         var numbersOfDigitsNeeded = NumberList.NaturalNumbers().First(
             numberOfDigits => numberOfDigits * 9.ToThePowerOf(exponent) <
                               NNinesAsNumber(numberOfDigits));
@@ -25,8 +27,6 @@ public class Problem0030 : IEulerProblem<BigInteger>
     {
         return Enumerable.Repeat(9, (int)numberOfDigits).Aggregate(0, (sum, digit) => sum * 10 + digit);
     }
-
-    public BigInteger Solution() => 0;
 
     private static Func<long, bool> IsSumOfNPowerOfDigits(int exponent)
     {
