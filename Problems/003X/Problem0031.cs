16aa6ca98f5815e7bdf6dce51566b4d3155712db
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-04-05 18:58:10 +0200

    Problem 0031: Refactoring

--- Problems/003X/Problem0031.cs
+++ Problems/003X/Problem0031.cs
@@ -13,19 +13,29 @@ public class Problem0031 : IEulerProblem<int>
             1, 2
         };
 
-        var possibleMonoCurrencyCombinations = currencyValues
-            .Select(
-                value => NumberList.NumbersBetween(0, targetValue / value).Select(count => count * value).ToList()).ToList();
+        var possibleMonoCurrencyCombinations = CreateMonoCurrencyCombinationsBotAbove(currencyValues, targetValue).ToList();
 
-        var combinationsValidOrNot = possibleMonoCurrencyCombinations.Aggregate<IEnumerable<long>, IEnumerable<long>>(
+        var combinationsValidOrNot = CreateCombinationsValidOrNot(possibleMonoCurrencyCombinations);
+
+        return combinationsValidOrNot
+            .Count(sum => sum == targetValue);
+    }
+
+    private static IEnumerable<List<long>> CreateMonoCurrencyCombinationsBotAbove(List<int> currencyValues, int targetValue)
+    {
+        return currencyValues
+            .Select(
+                value => NumberList.NumbersBetween(0, targetValue / value).Select(count => count * value).ToList());
+    }
+
+    private static List<long> CreateCombinationsValidOrNot(List<List<long>> possibleMonoCurrencyCombinations)
+    {
+        return possibleMonoCurrencyCombinations.Aggregate<IEnumerable<long>, IEnumerable<long>>(
             new List<long>{0},
             (uniqueSums, monoCurrencyCombinations) => uniqueSums.SelectMany(
                     uniqueSum =>
                         monoCurrencyCombinations.Select(singleValueSum => uniqueSum + singleValueSum))
                 .ToList()).ToList();
-
-        return combinationsValidOrNot
-            .Count(sum => sum == targetValue);
     }
 
     public int Solution() => 0;
