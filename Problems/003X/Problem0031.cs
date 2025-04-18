cb66e48b863a46b93c18ed6aec3218696180a87a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-04-18 06:38:46 +0200

    Problem 0031: Renaming

--- Problems/003X/Problem0031.cs
+++ Problems/003X/Problem0031.cs
@@ -9,35 +9,35 @@ public class Problem0031 : IEulerProblem<int>
         1, 2, 5, 10, 20, 100, 200
     };
 
-    public int Example() => CountPossibleCurrencyCombinationsLeadingToExact(2);
+    public int Example() => CountPossibleCoinCombinationsLeadingToExact(2);
 
     public int Solution() => 0;
 
-    private int CountPossibleCurrencyCombinationsLeadingToExact(int targetValue)
+    private int CountPossibleCoinCombinationsLeadingToExact(int targetValue)
     {
-        var possibleMonoCurrencyCombinations = CreateMonoCurrencyCombinationsBotAbove(_britishCountValuesInPence, targetValue).ToList();
+        var possibleMonoCoinCombinations = CreateMonoCoinCombinationsNotAbove(_britishCountValuesInPence, targetValue).ToList();
 
-        var combinationsValidOrNot = CreateCombinationsValidOrNot(possibleMonoCurrencyCombinations);
+        var combinationsValidOrNot = CreateCombinationsValidOrNot(possibleMonoCoinCombinations);
 
         return combinationsValidOrNot
             .Count(sum => sum == targetValue);
     }
 
-    private static IEnumerable<List<long>> CreateMonoCurrencyCombinationsBotAbove(IReadOnlyList<int> currencyValues,
+    private static IEnumerable<List<long>> CreateMonoCoinCombinationsNotAbove(IReadOnlyList<int> currencyValues,
         int targetValue) =>
         currencyValues
             .Select(
                 value => NumberList.NumbersBetween(0, targetValue / value).Select(count => count * value).ToList());
 
-    private static IEnumerable<long> CreateCombinationsValidOrNot(List<List<long>> possibleMonoCurrencyCombinations) =>
-        possibleMonoCurrencyCombinations.Aggregate<IEnumerable<long>, IEnumerable<long>>(
+    private static IEnumerable<long> CreateCombinationsValidOrNot(List<List<long>> possibleMonoCoinCombinations) =>
+        possibleMonoCoinCombinations.Aggregate<IEnumerable<long>, IEnumerable<long>>(
             new List<long> { 0 },
             CreatePossibleCombinations);
 
     private static IEnumerable<long> CreatePossibleCombinations(IEnumerable<long> uniqueSums,
-        IEnumerable<long> monoCurrencyCombinations) =>
+        IEnumerable<long> monoCoinCombinations) =>
         uniqueSums.SelectMany(
             uniqueSum =>
-                monoCurrencyCombinations.Select(singleValueSum => uniqueSum + singleValueSum));
+                monoCoinCombinations.Select(singleValueSum => uniqueSum + singleValueSum));
 
 }
