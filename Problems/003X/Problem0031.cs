2a6e0ab83b78c71aa4acd79cefe9c3b1cf65d2ed
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-04-18 06:28:01 +0200

    Problem 0031: Refactoring

--- Problems/003X/Problem0031.cs
+++ Problems/003X/Problem0031.cs
@@ -4,16 +4,16 @@ namespace Problems._003X;
 
 public class Problem0031 : IEulerProblem<int>
 {
+    private readonly IReadOnlyList<int> _currencyValues = new List<int>
+    {
+        1, 2, 5, 10, 20, 100, 200
+    };
+
     public int Example()
     {
         var targetValue = 2;
 
-        var currencyValues = new List<int>
-        {
-            1, 2, 5, 10, 20, 100, 200
-        };
-
-        var possibleMonoCurrencyCombinations = CreateMonoCurrencyCombinationsBotAbove(currencyValues, targetValue).ToList();
+        var possibleMonoCurrencyCombinations = CreateMonoCurrencyCombinationsBotAbove(_currencyValues, targetValue).ToList();
 
         var combinationsValidOrNot = CreateCombinationsValidOrNot(possibleMonoCurrencyCombinations);
 
@@ -21,7 +21,7 @@ public class Problem0031 : IEulerProblem<int>
             .Count(sum => sum == targetValue);
     }
 
-    private static IEnumerable<List<long>> CreateMonoCurrencyCombinationsBotAbove(List<int> currencyValues, int targetValue)
+    private static IEnumerable<List<long>> CreateMonoCurrencyCombinationsBotAbove(IReadOnlyList<int> currencyValues, int targetValue)
     {
         return currencyValues
             .Select(
