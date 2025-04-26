1f3e95127ad5d761e7c5f5b2db7da40942a2f20a
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-04-26 10:27:31 +0200

    Problem 0031: Implement Generator.cs

--- Utils.Tests/Data/GeneratorTests.cs
+++ Utils.Tests/Data/GeneratorTests.cs
@@ -4,11 +4,37 @@ namespace Utils.Tests.Data;
 
 public class GeneratorTests
 {
-    [Test]
-    public void Constructor_ShouldWork()
-    {
-        var generator = new Generator();
 
-        generator.Should().NotBeNull();
+    [Test]
+    public void First_ShouldReturnFirst()
+    {
+        var input = "first";
+        var generator = new Generator(input, s => throw new Exception());
+
+        var enumerable = generator.CreateEnumerable();
+
+        enumerable.First().Should().Be(input);
+    }
+
+    [Test]
+    public void Second_ShouldBeCreatedByUsingStepOnFirst()
+    {
+        var input = "first";
+        var generator = new Generator(input, s => s + "->next");
+
+        var enumerable = generator.CreateEnumerable();
+
+        enumerable.Skip(1).First().Should().Be("first->next");
+    }
+    
+    [Test]
+    public void Third_ShouldBeCreatedByUsingStepOnFirstTwoTimes()
+    {
+        var input = "first";
+        var generator = new Generator(input, s => s + "->next");
+
+        var enumerable = generator.CreateEnumerable();
+
+        enumerable.Skip(2).First().Should().Be("first->next->next");
     }
 }
--- Utils/Data/Generator.cs
+++ Utils/Data/Generator.cs
@@ -1,5 +1,20 @@
 ﻿namespace Utils.Data;
 
-public class Generator  
+public class Generator(string first, Func<string, string> step)
 {
+    public IEnumerable<string> CreateEnumerable()
+    {
+        var current = first;
+
+        yield return current;
+
+        while (true)
+        {
+            current = step(current);
+
+            yield return current;
+        }
+
+        // ReSharper disable once IteratorNeverReturns
+    }
 }
