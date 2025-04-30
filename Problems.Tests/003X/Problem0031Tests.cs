1ae694fa14b40b747fb7f52c8d5e92837b093e7d
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-04-30 18:57:06 +0200

    Problem 0031: Fix test

--- Problems.Tests/003X/Problem0031Tests.cs
+++ Problems.Tests/003X/Problem0031Tests.cs
@@ -5,5 +5,5 @@ namespace Problems.Tests._003X;
 public class Problem0031Tests : EulerProblemTestBase<Problem0031, int>
 {
     protected override int ExampleResult => 2;
-    protected override int ProblemResult => 0;
+    protected override int ProblemResult => 0x11FD2;
 }
