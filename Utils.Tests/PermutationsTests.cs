7f404e7408186a62472253e4363027a17de9ceec
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-06-07 09:41:35 +0200

    Problem 0032: Start Permutations class

---
+++ Utils.Tests/PermutationsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Utils.Tests;
+
+public class PermutationsTests
+{
+    [Test]
+    public void GetAsVolatile_NoElement_ShouldReturnEmpty()
+    {
+        var permutations = new Permutations(new HashSet<int>());
+
+        var list = permutations.GetAsVolatile()
+            .Select(array => array.ToList()).ToList();
+
+        list.Should().BeEquivalentTo(new List<List<int>>());
+    }
+
+    [Test]
+    public void GetAsVolatile_SingleElement_ShouldReturnSingleElement()
+    {
+        var permutations = new Permutations(new HashSet<int> { 1 });
+
+        var list = permutations.GetAsVolatile()
+            .Select(array => array.ToList()).ToList();
+
+        list.Should().BeEquivalentTo(
+            new List<List<int>>
+            {
+                new()
+                    { 1 }
+            });
+    }
+
+}
---
+++ Utils/Permutations.cs
@@ -0,0 +1,15 @@
+﻿namespace Utils;
+
+public class Permutations(ISet<int> hashSet)
+{
+
+    public IEnumerable<IReadOnlyCollection<int>> GetAsVolatile()
+    {
+        if (hashSet.Count == 0)
+        {
+            yield break;
+        }
+
+        yield return hashSet.ToArray();
+    }
+}
