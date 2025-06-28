5321016bd3a5dcbf84989b19be6ee77273082e8b
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-06-28 09:41:35 +0200

    Problem 0032: Express dummy problem using permutations

--- Problems/003X/Problem0032.cs
+++ Problems/003X/Problem0032.cs
@@ -1,8 +1,20 @@
-﻿namespace Problems._003X;
+﻿using Utils;
+
+namespace Problems._003X;
 
 public class Problem0032 : IEulerProblem<int>
 {
-    public int Example() => GetSumOfProducts([7254]);
+    public int Example()
+    {
+        var permutations = new Permutations(new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+        var list = new List<int> { 3, 9, 1, 8, 6, 7, 2, 5, 4 };
+        var hit = permutations.GetAsVolatile().Where(ints => ints.SequenceEqual(list)).Select(ints => ints.ToList()).Single();
+
+        var aggregate = hit.TakeLast(4).Aggregate(0, (current, nextDigit) => current * 10 + nextDigit);
+
+        return GetSumOfProducts([aggregate]);
+    }
 
     public int Solution() => 0;
 
