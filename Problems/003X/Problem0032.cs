a28aa962e452a14010df90c30daa58c2aac8f4ab
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-07-26 12:10:11 +0200

    Problem 0032: Refactoring

--- Problems/003X/Problem0032.cs
+++ Problems/003X/Problem0032.cs
@@ -7,10 +7,15 @@ public class Problem0032 : IEulerProblem<int>
 {
     public int Example()
     {
-        var permutations = new Permutations(new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-
         var list = new List<int> { 3, 9, 1, 8, 6, 7, 2, 5, 4 };
-        var hit = permutations.GetAsVolatile().Where(ints => ints.SequenceEqual(list)).Select(ints => ints.ToList()).Single();
+
+        return GetSumOfPandigitalProducts(ints => ints.SequenceEqual(list));
+    }
+
+    private static int GetSumOfPandigitalProducts(Func<IReadOnlyCollection<int>, bool> predicate)
+    {
+        var permutations = new Permutations(new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        var hit = permutations.GetAsVolatile().Where(predicate).Select(ints => ints.ToList()).Single();
 
         var potentialCandidates = hit.GetEverySplit().SelectMany(parts => parts.Second.GetEverySplit().Select(secondSplit =>
             (Multiplicand: parts.First.DigitsToNumber(),
