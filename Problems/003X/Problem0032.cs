4d33e746b649b51852a2e1106ab1c14d0d3c12ba
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-08-09 18:00:25 +0200

    Problem 0032: Refactoring

--- Problems/003X/Problem0032.cs
+++ Problems/003X/Problem0032.cs
@@ -7,23 +7,30 @@ public class Problem0032 : IEulerProblem<int>
 {
     public int Example() => GetSumOfPandigitalProducts(product => product == 7254);
 
+    public int Solution() => 0;
+
     private static int GetSumOfPandigitalProducts(Func<int, bool> allowedProducts)
     {
-        var permutations = new Permutations(new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }).GetAsVolatile();
+        var permutations = GetAllPermutationsFromOneToNine();
 
-        var potentialCandidates = permutations.SelectMany(permutation => permutation.GetEverySplit()).SelectMany(parts =>
-            parts.Second.GetEverySplit().Select(secondSplit =>
-                (Multiplicand: parts.First.DigitsToNumber(),
-                    Multiplier: secondSplit.First.DigitsToNumber(),
-                    Product: secondSplit.Second.DigitsToNumber())));
+        var potentialCandidates = GetPotentialCandidates(permutations);
 
-        var pandigitalProduct = potentialCandidates
-            .Where(candidate => candidate.Multiplicand * candidate.Multiplier == candidate.Product).ToList();
+        var pandigitalProduct = potentialCandidates.Where(IsValidProduct).ToList();
 
         return pandigitalProduct.Select(candidate => candidate.Product).Distinct().Where(allowedProducts).Sum();
     }
 
-    public int Solution() => 0;
+    private static IEnumerable<IReadOnlyCollection<int>> GetAllPermutationsFromOneToNine() =>
+        new Permutations(new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }).GetAsVolatile();
 
+    private static bool IsValidProduct((int Multiplicand, int Multiplier, int Product) candidate) =>
+        candidate.Multiplicand * candidate.Multiplier == candidate.Product;
+
+    private static IEnumerable<(int Multiplicand, int Multiplier, int Product)> GetPotentialCandidates(
+        IEnumerable<IReadOnlyCollection<int>> permutations) =>
+        permutations.SelectMany(permutation => permutation.GetEverySplit()).SelectMany(parts =>
+            parts.Second.GetEverySplit().Select(secondSplit =>
+                (Multiplicand: parts.First.DigitsToNumber(),
+                    Multiplier: secondSplit.First.DigitsToNumber(),
+                    Product: secondSplit.Second.DigitsToNumber())));
 }
- 
