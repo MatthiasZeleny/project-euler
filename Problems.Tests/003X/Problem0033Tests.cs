820d6bbe9413cbf16e42ad0ce2d7b3bd0928f5cf
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-10-11 09:37:45 +0200

    Problem 0033: Create dummy problem

---
+++ Problems.Tests/003X/Problem0033Tests.cs
@@ -0,0 +1,9 @@
+﻿using Problems._003X;
+
+namespace Problems.Tests._003X;
+
+public class Problem0033Tests : EulerProblemTestBase<Problem0033, int>
+{
+    protected override int ExampleResult => 2;
+    protected override int ProblemResult => 0;
+}
---
+++ Problems/003X/Problem0033.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._003X;
+
+public class Problem0033 : IEulerProblem<int>
+{
+    public int Example() => 2;
+
+    public int Solution() => 0;
+}
