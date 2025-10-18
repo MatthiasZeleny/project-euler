ae1c3df232fd1c4fff858cca97979e64210f830e
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2025-10-18 17:52:01 +0200

    Problem 0033: Add Fraction class

---
+++ Numbers.Tests/BasicMath/FractionTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Numbers.BasicMath;
+
+namespace Numbers.Tests.BasicMath;
+
+public class FractionTests
+{
+
+    [Test]
+    public void Denominator_ShouldContainValueOfDenominator()
+    {
+        var fraction = new Fraction(1, 2);
+
+        fraction.Denominator.Should().Be(2);
+    }
+
+    [Test]
+    public void Numerator_ShouldContainValueOfNumerator()
+    {
+        var fraction = new Fraction(1, 2);
+
+        fraction.Numerator.Should().Be(1);
+    }
+
+    [Test]
+    public void HasSameValue_SameObject_ShouldReturnTrue()
+    {
+        var fraction = new Fraction(3,6);
+
+        var hasSameValueAs = fraction.HasSameValueAs(fraction);
+        
+        hasSameValueAs.Should().BeTrue();
+    }
+
+
+    [Test]
+    public void HasSameValue_SameValues_ShouldReturnTrue()
+    {
+        var fraction = new Fraction(3,6);
+        var otherFraction = new Fraction(3,6);
+
+        var hasSameValueAs = fraction.HasSameValueAs(otherFraction);
+        
+        hasSameValueAs.Should().BeTrue();
+    }
+
+    [Test]
+    public void HasSameValue_DifferentQuotient_ShouldReturnTrue()
+    {
+        var fraction = new Fraction(3,6);
+        var otherFraction = new Fraction(2,6);
+
+        var hasSameValueAs = fraction.HasSameValueAs(otherFraction);
+        
+        hasSameValueAs.Should().BeFalse();
+    }
+
+    [Test]
+    public void HasSameValue_SameQuotient_ShouldReturnTrue()
+    {
+        var fraction = new Fraction(3,6);
+        var otherFraction = new Fraction(1,2);
+
+        var hasSameValueAs = fraction.HasSameValueAs(otherFraction);
+        
+        hasSameValueAs.Should().BeTrue();
+    }
+}
---
+++ Numbers/BasicMath/Fraction.cs
@@ -0,0 +1,19 @@
+﻿namespace Numbers.BasicMath;
+
+public class Fraction
+{
+    public Fraction(int numerator, int denominator)
+    {
+        Numerator = numerator;
+        Denominator = denominator;
+    }
+
+    public int Numerator { get; }
+    public int Denominator { get; }
+
+
+    public bool HasSameValueAs(Fraction otherFraction)
+    {
+        return Numerator * otherFraction.Denominator == Denominator * otherFraction.Numerator;
+    }
+}
