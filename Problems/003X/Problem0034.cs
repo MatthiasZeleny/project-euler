b4915285d4287c816466cd27d18f52fab875daef
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-01-31 06:35:40 +0100

    Problem 0033: Add factorial sum check to example

--- Problems/003X/Problem0034.cs
+++ Problems/003X/Problem0034.cs
@@ -1,11 +1,21 @@
-﻿namespace Problems._003X;
+﻿using Numbers.BasicMath;
+
+namespace Problems._003X;
 
 /// <summary>
 /// https://projecteuler.net/problem=34
 /// </summary>
 public class Problem0034 : IEulerProblem<long>
 {
-    public long Example() => 145;
+    public long Example() => new List<long> { 145 }
+        .Where(number => number.ToDigitList().Select(Factorial).Sum() == number).Sum();
 
     public long Solution() => 0;
+
+    private static long Factorial(long digit)
+    {
+        if (digit == 0) return 1;
+
+        return digit * Factorial(digit - 1);
+    }
 }
