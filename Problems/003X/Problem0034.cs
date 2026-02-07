0c40e8dfdf119f3e1eb6ad491421c3cb6df2bfe0
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-02-07 09:12:19 +0100

    Problem 034: Solve problem

--- Problems/003X/Problem0034.cs
+++ Problems/003X/Problem0034.cs
@@ -10,7 +10,22 @@ public class Problem0034 : IEulerProblem<long>
     public long Example() => new List<long> { 145 }
         .Where(number => number.ToDigitList().Select(Factorial).Sum() == number).Sum();
 
-    public long Solution() => 0;
+    public long Solution()
+    {
+        var maxDigitCount = NumberList.NaturalNumbers()
+            .TakeWhile(digitCount => digitCount * Factorial(9) > TenToThePowerOf(digitCount))
+            .Last() + 1;
+
+        return NumberList.NumbersBetween(3, TenToThePowerOf(maxDigitCount))
+            .Where(number => number.ToDigitList().Select(Factorial).Sum() == number).Sum();
+    }
+
+    private long TenToThePowerOf(long digitCount)
+    {
+        if (digitCount == 0) return 1;
+
+        return 10 * TenToThePowerOf(digitCount - 1);
+    }
 
     private static long Factorial(long digit)
     {
