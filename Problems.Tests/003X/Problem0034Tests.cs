4bf2e7a872c2481ff169d6e6573a42c16935c7f0
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-02-14 06:36:19 +0100

    Problem 0034: Use hex for solution value

--- Problems.Tests/003X/Problem0034Tests.cs
+++ Problems.Tests/003X/Problem0034Tests.cs
@@ -5,5 +5,5 @@ namespace Problems.Tests._003X;
 public class Problem0034Tests : EulerProblemTestBase<Problem0034, long>
 {
     protected override long ExampleResult => 145;
-    protected override long ProblemResult => 40730;
+    protected override long ProblemResult => 0x9F1A;
 }
