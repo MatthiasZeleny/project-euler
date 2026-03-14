d59c6ecb7aa5ffb965cb724cc5581c4b6c411d34
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-03-14 06:36:39 +0100

    Problem 0034: Create dummy problem

---
+++ Problems.Tests/003X/Problem0035Tests.cs
@@ -0,0 +1,9 @@
+﻿using Problems._003X;
+
+namespace Problems.Tests._003X;
+
+public class Problem0035Tests : EulerProblemTestBase<Problem0035, long>
+{
+    protected override long ExampleResult => 1;
+    protected override long ProblemResult => 0;
+}
---
+++ Problems/003X/Problem0035.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._003X;
+
+public class Problem0035 : IEulerProblem<long>
+{
+    public long Example() => new List<int> { 197 }.Count;
+
+    public long Solution() => 0;
+}
