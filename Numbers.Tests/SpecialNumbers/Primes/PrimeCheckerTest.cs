8e65aa9b512bd16a994910e1285a366266f75e9c
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-03-21 06:32:06 +0100

    Problem 0034: Add PrimeChecker

---
+++ Numbers.Tests/SpecialNumbers/Primes/PrimeCheckerTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Numbers.SpecialNumbers.Primes;
+
+namespace Numbers.Tests.SpecialNumbers.Primes;
+
+[TestFixture]
+public class PrimeCheckerTest
+{
+
+    [Test]
+    [TestCase(1, false)]
+    [TestCase(2, true)]
+    [TestCase(3, true)]
+    [TestCase(4, false)]
+    public void IsPrime_ShouldReturnExpectedResult(int candidate, bool expected)
+    {
+        var primeChecker = new PrimeChecker();
+        
+        var isPrime = primeChecker.IsPrime(candidate);
+
+        isPrime.Should().Be(expected);
+    }
+}
---
+++ Numbers/SpecialNumbers/Primes/PrimeChecker.cs
@@ -0,0 +1,17 @@
+﻿using Utils.Data;
+
+namespace Numbers.SpecialNumbers.Primes;
+
+public class PrimeChecker
+{
+    private readonly CachingEnumerator _primes = new(Prime.Create());
+    private const long NoPrimeValue = -1;
+
+    public bool IsPrime(int candidate)
+    {
+        var maybePrime = _primes.GetElements().SkipWhile(prime => prime < candidate).TakeWhile(prime => prime == candidate)
+            .FirstOrDefault(NoPrimeValue);
+
+        return maybePrime != NoPrimeValue;
+    }
+}
