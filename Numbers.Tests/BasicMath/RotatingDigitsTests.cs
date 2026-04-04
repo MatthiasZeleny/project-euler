279add0d22601f85cf313014a75bc4ec84016dc3
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-04-04 06:21:06 +0200

    Problem 0034: Start implementing RotatingDigits

---
+++ Numbers.Tests/BasicMath/RotatingDigitsTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Numbers.BasicMath;
+
+namespace Numbers.Tests.BasicMath;
+
+public class RotatingDigitsTests
+{
+    [TestCase(1, 1)]
+    public void From_ShouldReturnExpected(long number, params long[] expected)
+    {
+        var rotation = RotatingDigits.From(number);
+
+        rotation.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Numbers/BasicMath/RotatingDigits.cs
@@ -0,0 +1,9 @@
+﻿namespace Numbers.BasicMath;
+
+public static class RotatingDigits
+{
+    public static IEnumerable<long> From(long number)
+    {
+        return [number];
+    }
+}
