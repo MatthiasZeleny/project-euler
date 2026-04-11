79fdbc6f8feacb7dc3b1da85e7f405621bcf0a26
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-04-11 08:34:17 +0200

    Problem 0034: Finish implementing RotatingDigits

--- Numbers.Tests/BasicMath/RotatingDigitsTests.cs
+++ Numbers.Tests/BasicMath/RotatingDigitsTests.cs
@@ -6,6 +6,8 @@ namespace Numbers.Tests.BasicMath;
 public class RotatingDigitsTests
 {
     [TestCase(1, 1)]
+    [TestCase(12, 12, 21)]
+    [TestCase(123, 123, 231, 312)]
     public void From_ShouldReturnExpected(long number, params long[] expected)
     {
         var rotation = RotatingDigits.From(number);
--- Numbers/BasicMath/RotatingDigits.cs
+++ Numbers/BasicMath/RotatingDigits.cs
@@ -4,6 +4,22 @@ public static class RotatingDigits
 {
     public static IEnumerable<long> From(long number)
     {
-        return [number];
+        var digits = number.ToDigitList().ToList();
+
+        var allCombinations = digits.Select((_, index) =>
+        {
+            var start = digits.Take(index);
+
+            var end =digits.Skip(index);
+            
+            return end.Concat(start);
+        });
+
+        return allCombinations.Select(DigitsToLong);
+    }
+
+    private static long DigitsToLong(IEnumerable<long> list)
+    {
+        return list.Aggregate(0L, (sum, digit)=> sum*10+digit);
     }
 }
