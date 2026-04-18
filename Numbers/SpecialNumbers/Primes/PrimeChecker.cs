3b7b246827e9b811e21302c12514e3d94aa67860
Matthias Zeleny <mattihas.zeleny+github@gmail.com>
2026-04-18 08:09:26 +0200

    Problem 0035: Update dummy problem

--- Numbers/SpecialNumbers/Primes/PrimeChecker.cs
+++ Numbers/SpecialNumbers/Primes/PrimeChecker.cs
@@ -7,7 +7,7 @@ public class PrimeChecker
     private readonly CachingEnumerator _primes = new(Prime.Create());
     private const long NoPrimeValue = -1;
 
-    public bool IsPrime(int candidate)
+    public bool IsPrime(long candidate)
     {
         var maybePrime = _primes.GetElements().SkipWhile(prime => prime < candidate).TakeWhile(prime => prime == candidate)
             .FirstOrDefault(NoPrimeValue);
--- Problems/003X/Problem0035.cs
+++ Problems/003X/Problem0035.cs
@@ -1,8 +1,16 @@
-﻿namespace Problems._003X;
+﻿using Numbers.BasicMath;
+using Numbers.SpecialNumbers.Primes;
+
+namespace Problems._003X;
 
 public class Problem0035 : IEulerProblem<long>
 {
-    public long Example() => new List<int> { 197 }.Count;
+    public long Example()
+    {
+        var primeChecker = new PrimeChecker();
+
+        return new List<int> { 197 }.Select(number=> RotatingDigits.From(number)).Count(rotation => rotation.All(candidate=>primeChecker.IsPrime(candidate)));
+    }
 
     public long Solution() => 0;
 }
