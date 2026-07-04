b1984e5b3e889a86198090c532a64fa147f5350c
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-07-04 17:55:18 +0200

    Problem 0036: Use palindrome check for example

--- Problems/003X/Problem0036.cs
+++ Problems/003X/Problem0036.cs
@@ -1,8 +1,13 @@
-﻿namespace Problems._003X;
+﻿using Numbers.BasicMath;
+
+namespace Problems._003X;
 
 public class Problem0036 : IEulerProblem<long>
 {
-    public long Example() => new List<int> { 585 }.Count;
+    public long Example() => new List<long> { 585 }
+        .Count(IsBaseTenAndBaseTwoBaIsPalindrome);
+
+    private static bool IsBaseTenAndBaseTwoBaIsPalindrome(long number) => number.IsPalindrome(10) && number.IsPalindrome(2);
 
     public long Solution() => 0;
 }
