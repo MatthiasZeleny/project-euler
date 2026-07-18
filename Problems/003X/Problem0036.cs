9439ad336542f4cc26390819d2237b1b55404620
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-07-18 13:43:45 +0200

    Problem 0036: Extract methods and refactor

--- Problems/003X/Problem0036.cs
+++ Problems/003X/Problem0036.cs
@@ -4,12 +4,21 @@ namespace Problems._003X;
 
 public class Problem0036 : IEulerProblem<long>
 {
-    public long Example() => new List<long> { 585 }
-        .Where(IsBaseTenAndBaseTwoBaIsPalindrome).Sum();
+    private const long ExampleValue = 585;
 
-    private static bool IsBaseTenAndBaseTwoBaIsPalindrome(long number) => number.IsPalindrome(10) && number.IsPalindrome(2);
+    public long Example() => new List<long> { ExampleValue }
+        .Where(IsBaseTenAndBaseTwoPalindrome).Sum();
 
-    public long Solution() => NumberList.NumbersBetween(1,1_000_000)
-        .Where(IsBaseTenAndBaseTwoBaIsPalindrome)
+    public long Solution() => NumbersBelowOneMillion()
+        .Where(IsBaseTenAndBaseTwoPalindrome)
         .Sum();
+
+    private static IEnumerable<long> NumbersBelowOneMillion() => NumberList.NumbersBetween(1, 1_000_000);
+
+    private static bool IsBaseTenAndBaseTwoPalindrome(long number) =>
+        IsBaseTenPalindrome(number) && IsBinaryPalindrome(number);
+
+    private static bool IsBinaryPalindrome(long number) => number.IsPalindrome(2);
+
+    private static bool IsBaseTenPalindrome(long number) => number.IsPalindrome(10);
 }
