644e324ed18a320826a559cd149bc646cc671d32
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-07-26 09:18:26 +0200

    Problem 0037: Add trivial example

---
+++ Problems.Tests/003X/Problem0037Tests.cs
@@ -0,0 +1,9 @@
+﻿using Problems._003X;
+
+namespace Problems.Tests._003X;
+
+public class Problem0037Tests : EulerProblemTestBase<Problem0037, long>
+{
+    protected override long ExampleResult => 3797;
+    protected override long ProblemResult => 0x0;
+}
---
+++ Problems/003X/Problem0037.cs
@@ -0,0 +1,8 @@
+﻿namespace Problems._003X;
+
+public class Problem0037 : IEulerProblem<long>
+{
+    public long Example() => 3797;
+
+    public long Solution() => 0;
+}
