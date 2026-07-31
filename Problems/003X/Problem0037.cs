15fae19dd0164a02872d2fb662dd8921542b4038
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-07-31 17:48:37 +0200

    Problem 0037: Use prime checker

--- Problems/003X/Problem0037.cs
+++ Problems/003X/Problem0037.cs
@@ -1,8 +1,11 @@
-﻿namespace Problems._003X;
+﻿using Numbers.SpecialNumbers.Primes;
+
+namespace Problems._003X;
 
 public class Problem0037 : IEulerProblem<long>
 {
-    public long Example() => 3797;
+    private static readonly PrimeChecker PrimeChecker = new();
+    public long Example() => new List<long> { 3797 }.Where(number => PrimeChecker.IsPrime(number)).Sum();
 
     public long Solution() => 0;
 }
