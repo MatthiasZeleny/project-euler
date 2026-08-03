3d81bd7973d3a4636b0e98b274dfdd82c1a23b08
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-08-03 17:59:32 +0200

    Problem 0037: Extract method

--- Problems/003X/Problem0037.cs
+++ Problems/003X/Problem0037.cs
@@ -5,7 +5,9 @@ namespace Problems._003X;
 public class Problem0037 : IEulerProblem<long>
 {
     private static readonly PrimeChecker PrimeChecker = new();
-    public long Example() => new List<long> { 3797 }.Where(number => PrimeChecker.IsPrime(number)).Sum();
-
+    public long Example() => new List<long> { 3797 }.Where(IsValidNumber).Sum();
     public long Solution() => 0;
+
+    private static bool IsValidNumber(long number) => IsPrime(number);
+    private static bool IsPrime(long number) => PrimeChecker.IsPrime(number);
 }
