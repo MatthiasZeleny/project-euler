1c3eba057048648a26507e34475b576455a08af7
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-08-10 18:06:50 +0200

    Problem 0037: Start implementing helper class for TrunctablePrimes

---
+++ Numbers.Tests/SpecialNumbers/Primes/SpecialPrimes/TrunctablePrimesTest.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Numbers.SpecialNumbers.Primes;
+using Numbers.SpecialNumbers.Primes.SpecialPrimes;
+
+namespace Numbers.Tests.SpecialNumbers.Primes.SpecialPrimes;
+
+[TestFixture]
+public class TrunctablePrimesTest
+{
+    private static readonly PrimeChecker PrimeChecker = new();
+
+    [Test]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(5)]
+    [TestCase(7)]
+    public void IsTrunctablePrime_SingleDigitPrime_ShouldReturnFalse(long number)
+    {
+        PrimeChecker.IsPrime(number).Should().BeTrue();
+        
+        var result = number.IsTrunctablePrime(PrimeChecker);
+        
+        result.Should().BeFalse("defined as not part of the definition in problem 37.");
+    }
+    [Test]
+    [TestCase(1)]
+    [TestCase(4)]
+    public void IsTrunctablePrime_NonPrime_ShouldReturnFalse(long number)
+    {
+        PrimeChecker.IsPrime(number).Should().BeFalse();
+        
+        var result = number.IsTrunctablePrime(PrimeChecker);
+        
+        result.Should().BeFalse();
+    }
+}
---
+++ Numbers/SpecialNumbers/Primes/SpecialPrimes/TrunctablePrimes.cs
@@ -0,0 +1,10 @@
+﻿namespace Numbers.SpecialNumbers.Primes.SpecialPrimes;
+
+public static class TrunctablePrimes
+{
+
+    public static bool IsTrunctablePrime(this long number, PrimeChecker primeChecker)
+    {
+        return false;
+    }
+}
