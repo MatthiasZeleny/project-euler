f073c9db72971106916d72fc49084c3ed36eda84
Matthias Zeleny <matthias.zeleny@gmail.com>
2026-08-18 17:46:56 +0200

    Problem 0037: Add a negative test case

--- Numbers.Tests/SpecialNumbers/Primes/SpecialPrimes/TrunctablePrimesTest.cs
+++ Numbers.Tests/SpecialNumbers/Primes/SpecialPrimes/TrunctablePrimesTest.cs
@@ -17,20 +17,32 @@ public class TrunctablePrimesTest
     public void IsTrunctablePrime_SingleDigitPrime_ShouldReturnFalse(long number)
     {
         PrimeChecker.IsPrime(number).Should().BeTrue();
-        
+
         var result = number.IsTrunctablePrime(PrimeChecker);
-        
+
         result.Should().BeFalse("defined as not part of the definition in problem 37.");
     }
+
     [Test]
     [TestCase(1)]
     [TestCase(4)]
     public void IsTrunctablePrime_NonPrime_ShouldReturnFalse(long number)
     {
         PrimeChecker.IsPrime(number).Should().BeFalse();
-        
+
         var result = number.IsTrunctablePrime(PrimeChecker);
-        
+
+        result.Should().BeFalse();
+    }
+
+    [Test]
+    [TestCase(11)]
+    public void IsTrunctablePrime_TwoDigitNonTruncatablePrime_ShouldReturnFalse(long number)
+    {
+        PrimeChecker.IsPrime(number).Should().BeTrue();
+
+        var result = number.IsTrunctablePrime(PrimeChecker);
+
         result.Should().BeFalse();
     }
 }
